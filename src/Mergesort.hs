119e504d3e31b9c55b85ec0ac77e83064ff35b8d
kaBeech <contact@kaBeech.com>
2024-04-15 23:33:28 -0700

    Improve efficiency
    If there are only two elements in a branch in initial breakdown,
    merge them immedialtely and keep the pairs to prevent having to
    do another pass while breaking down

--- src/Mergesort.hs
+++ src/Mergesort.hs
@@ -1,16 +1,17 @@
 module Mergesort (msort) where
 
-msort :: (Ord arraelement2) => [arraelement2] -> [arraelement2]
+msort :: (Ord array) => [array] -> [array]
 msort = mergeAll . map (: [])
   where
     mergeAll [] = []
     mergeAll [singleElement] = singleElement
+    mergeAll [element1, element2] = merge element1 element2
     mergeAll remaningElements = mergeAll (mergePairs remaningElements)
 
     mergePairs (element1 : element2 : remaningElements) = merge element1 element2 : mergePairs remaningElements
     mergePairs remaningElements = remaningElements
 
-merge :: (Ord arraelement2) => [arraelement2] -> [arraelement2] -> [arraelement2]
+merge :: (Ord array) => [array] -> [array] -> [array]
 merge [] remainingElements2 = remainingElements2
 merge remainingElements1 [] = remainingElements1
 merge (element1 : remainingElements1) (element2 : remainingElements2)
