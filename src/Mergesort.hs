ebfa177a3285f49892884bcd53328b69c84dc515
kaBeech <contact@kaBeech.com>
2024-04-16 00:03:10 -0700

    Improve clarity

--- src/Mergesort.hs
+++ src/Mergesort.hs
@@ -12,8 +12,8 @@ msort = mergeAll . map (: [])
     mergePairs remaningElements = remaningElements
 
 merge :: (Ord array) => [array] -> [array] -> [array]
-merge [] remainingElements2 = remainingElements2
-merge remainingElements1 [] = remainingElements1
+merge [] element2 = element2
+merge element1 [] = element1
 merge (element1 : remainingElements1) (element2 : remainingElements2)
   | element1 < element2 = element1 : merge remainingElements1 (element2 : remainingElements2)
   | otherwise = element2 : merge (element1 : remainingElements1) remainingElements2
