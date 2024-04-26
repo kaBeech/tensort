92fe70d9d545dfb0df054b20ca93b3ee2df95143
kaBeech <contact@kaBeech.com>
2024-04-25 20:06:36 -0700

    Move pseudorandom generators to their own module

--- src/CoinFlip.hs
+++ src/CoinFlip.hs
@@ -1,23 +1,21 @@
 module CoinFlip (flipTenCoins) where
 
 import Control.Monad.State
-import System.Random (Random (random), RandomGen, StdGen, mkStdGen)
-
-randomSt :: (RandomGen g, Random a) => State g a
-randomSt = state random
+import Pseudorandom (rollPercentChance)
+import System.Random (StdGen, mkStdGen)
 
 tenCoins :: State StdGen (Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool)
 tenCoins = do
-  a <- randomSt
-  b <- randomSt
-  c <- randomSt
-  d <- randomSt
-  e <- randomSt
-  f <- randomSt
-  g <- randomSt
-  h <- randomSt
-  i <- randomSt
-  j <- randomSt
+  a <- rollPercentChance 50
+  b <- rollPercentChance 50
+  c <- rollPercentChance 50
+  d <- rollPercentChance 50
+  e <- rollPercentChance 50
+  f <- rollPercentChance 50
+  g <- rollPercentChance 50
+  h <- rollPercentChance 50
+  i <- rollPercentChance 50
+  j <- rollPercentChance 50
   return (a, b, c, d, e, f, g, h, i, j)
 
 flipTenCoins :: Int -> (Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool)
--- src/Wonky.hs
+++ src/Wonky.hs
@@ -1,25 +1,13 @@
 module Wonky (trinaryCompare) where
 
 import Control.Monad.State
-import System.Random (Random (random), RandomGen, StdGen, mkStdGen)
-
-randomSt :: (RandomGen g, Random a) => State g a
-randomSt = state random
-
-getRandomNumber :: Int -> State StdGen Int
-getRandomNumber maxNumber = do
-  randomNumber <- randomSt
-  return (1 + (randomNumber `mod` maxNumber))
+import Pseudorandom (getRandomNumber, rollPercentChance)
+import System.Random (StdGen, mkStdGen)
 
 compareRandomly :: State StdGen Int
 compareRandomly = do
   randomResult <- getRandomNumber 3
   return (randomResult - 2)
-
-rollPercentChance :: Int -> State StdGen Bool
-rollPercentChance percentage = do
-  roll <- getRandomNumber 100
-  return (roll <= percentage)
 
 trinaryCompare :: (Ord a) => a -> a -> Int -> Int -> Int
 trinaryCompare x y wonkinessPercentage seed = do
