77e2e7ea083909781cadcfcfc56a8aa3a2f4f9e2
kaBeech <contact@kaBeech.com>
2024-04-25 19:05:35 -0700

    Refactor to use comparator in preparation for Wonky

--- src/Bubblesort.hs
+++ src/Bubblesort.hs
@@ -5,4 +5,7 @@ bsort = foldr bubblesortSinglePass []
 
 bubblesortSinglePass :: (Ord array) => array -> [array] -> [array]
 bubblesortSinglePass element1 [] = [element1]
-bubblesortSinglePass element1 (element2 : remaningElements) = min element1 element2 : bubblesortSinglePass (max element1 element2) remaningElements
+bubblesortSinglePass element1 (element2 : remaningElements) = do
+  if element1 < element2
+    then element1 : bubblesortSinglePass element2 remaningElements
+    else element2 : bubblesortSinglePass element1 remaningElements
