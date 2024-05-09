617ce93fff1dc05dd84974908a9bd101bf1301ee
kaBeech <contact@kaBeech.com>
2024-05-08 20:49:47 -0700

    Add initial TypeScript stuff

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,8 @@
 module Main where
 
 import Bubblesort qualified (bubblesort)
-import CoinFlip qualified (flipTenCoins)
 import Mergesort qualified (mergesort)
 import Quicksort qualified (quicksort)
-import Wonky qualified (trinaryCompare)
 
 unsortedArray :: [Int]
 unsortedArray = [2, 5, 10, 4, 15, 11, 7, 14, 16, 6, 13, 3, 8, 9, 12, 1]
@@ -15,5 +13,3 @@ main = do
   putStrLn ("bubblesort: " ++ show (Bubblesort.bubblesort unsortedArray))
   putStrLn ("mergesort: " ++ show (Mergesort.mergesort unsortedArray))
   putStrLn ("quicksort: " ++ show (Quicksort.quicksort unsortedArray))
-  putStrLn ("Flip Ten Coins: " ++ show (CoinFlip.flipTenCoins 3343))
-  putStrLn ("Trinary Compare: " ++ show (Wonky.trinaryCompare 1 2 50 345))
