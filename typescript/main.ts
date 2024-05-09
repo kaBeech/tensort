ee7a3ded97119369686f5f2cada7443ac6041391
kaBeech <contact@kaBeech.com>
2024-05-08 22:48:19 -0700

    Cleanup

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,13 +1,5 @@
 import { bubblesort } from "./bubblesort.ts";
 
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
 let unsortedArray = [5, 3, 1, 4, 6, 2, 0, 7, 9, 8];
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-  console.log(bubblesort(unsortedArray));
-}
+console.log(bubblesort(unsortedArray));
