a06ea21c483f6af89f7dccba44a2c06903bbac35
kaBeech <contact@kaBeech.com>
2024-05-20 14:14:19 -0700

    Remove TypeScript code and move Haskell work to rood dir

--- typescript/main.ts
+++
@@ -1,9 +0,0 @@
-import bubblesort from "./bubblesort.ts";
-import { setRandomComparitorPercentage } from "./globalState.ts";
-import unsortedArray from "./unsortedArray.ts";
-
-console.log(bubblesort(unsortedArray()));
-
-setRandomComparitorPercentage(50);
-
-console.log(bubblesort(unsortedArray()));
