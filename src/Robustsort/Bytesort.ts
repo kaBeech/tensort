57d4c24eb3d05954f330fccd46256f4a69c4843c
kaBeech <contact@kaBeech.com>
2024-05-20 22:53:15 -0700

    Finish logic for composition steps

--- src/Robustsort/Bytesort.ts
+++ src/Robustsort/Bytesort.ts
@@ -4,26 +4,30 @@ type Byte = Bit[]
 
 interface Bytemap {
     pointers: number[],
-    bytes: Byte[],
+    data: Byte[],
 }
 
 type Metabyte = Bytemap | Metabytemap
 
 interface Metabytemap {
     pointers: number[],
-    metabytes: Metabyte[],
+    data: Metabyte[],
 }
 
 interface Package {
     reference: number,
-    value: Bit
+    bit: Bit
 }
 
 type Bytestack = Metabyte
 
 type Stackmap = Metabytemap
 
+// Dummy function in place of a true bubblesort
 const bubblesort = (a: number[]) => a.sort()
+
+// Dummy function in place of a true bubblesort
+const bubblesortPackages = (packages: Package[]) => packages.sort((a, b) => a.bit - b.bit)
 
 const bytesort4Bit = (a: number[]) => bytesort(a, 4)
 
@@ -44,7 +48,7 @@ const bytesort = (bits: number[], bytesize: number): number[] => {
     const bytes = convertRawBitsToBytes(bits, bytesize)
     let bytestacks = getBytestacksFromBytes(bytes)
     while (bytestacks.length > bytesize) {
-        bytestacks = reduceBytestacks(bytestacks)
+        bytestacks = reduceBytestacks(bytestacks, 4)
     }
     return getSortedArrayFromBytestacks(bytestacks)
 }
@@ -76,11 +80,39 @@ const getBytemapFromBytes = (bytes: Byte[]): Bytemap => {
         pointers.push(i)
         i++
     }
-    return { pointers, bytes }
+    return { pointers, data: bytes }
 }
 
-const reduceBytestacks = (bytestacks: Bytestack[]): Bytestack[] => {
-    return bytestacks
+const reduceBytestacks = (bytestacks: Bytestack[], bytesize: number): Bytestack[] => {
+    const newBytestacks: Bytestack[] = []
+    while (bytestacks.length > 0) {
+        const bytestack = createBytestack(bytestacks.slice(0, bytesize))
+        newBytestacks.push(bytestack)
+        bytestacks = bytestacks.slice(bytesize)
+    }
+    return newBytestacks
+}
+
+const createBytestack = (metabytes: Metabyte[]): Bytestack => {
+    const packages: Package[] = []
+    let i = 0
+    for (const metabyte of metabytes) {
+        packages.push({ reference: i, bit: getTopValueFromBytestack(metabyte) })
+    }
+    let packagesSorted = bubblesortPackages(packages)
+    const pointers: number[] = []
+    for (const package of packagesSorted) {
+        pointers.push(package.reference)
+    }
+    return { pointers, data: metabytes }
+}
+
+const getTopValueFromBytestack = (bytestack: Bytestack): number => {
+    const topByteOrMetabyte: Byte | Metabyte = bytestack.data[bytestack.pointers[bytestack.pointers.length - 1]]
+    if (typeof topByteOrMetabyte[0] == "number") {
+        const topByte = topByteOrMetabyte as Byte
+        return topByte[topByte.length - 1]
+    } else return getTopValueFromBytestack(topByteOrMetabyte as Metabyte)
 }
 
 const getSortedArrayFromBytestacks = (bytestacks: Bytestack[]) => {
