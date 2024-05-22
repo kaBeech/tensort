986260cf4572e33eac4d492cfdbc8a1fe04acd92
kaBeech <contact@kaBeech.com>
2024-05-22 12:56:21 -0700

    Add top-level bytesort logic

--- src/Data/Robustsort/Bytesort.hs
+++ src/Data/Robustsort/Bytesort.hs
@@ -1,5 +1,17 @@
 -- See `bytesort.ts` for (pseudo)code
-module Data.Robustsort.Bytesort (bytesort) where
+module Data.Robustsort.Bytesort (bytesort, bytesort4Bit) where
 
-bytesort :: [Int] -> [Int]
-bytesort xs = xs
+import Data.Robustsort.Subalgorithms.Bubblesort (bubblesort)
+import Data.Robustsort.Utils.Bytes (convertRawBitsToBytes, getBytestacksFromBytes, getSortedArrayFromBytestacks, reduceBytestacks)
+import Data.Robustsort.Utils.RandomizeList (randomizeList)
+
+bytesort4Bit :: [Int] -> [Int]
+bytesort4Bit xs = bytesort xs 4
+
+bytesort :: [Int] -> Int -> [Int]
+bytesort xs bytesize = do
+  let bits = randomizeList xs
+  let bytes = convertRawBitsToBytes bits bytesize
+  let bytestacks = getBytestacksFromBytes bytes bytesize
+  let bytestacks' = reduceBytestacks bytestacks bytesize
+  getSortedArrayFromBytestacks bytestacks'
