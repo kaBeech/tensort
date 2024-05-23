3a39a4904e9d3fe1a07f3140e818faf6e9f5c9a1
kaBeech <contact@kaBeech.com>
2024-05-23 02:00:25 -0700

    Update naming

--- src/Data/Robustsort/Bytesort.hs
+++ src/Data/Robustsort/Bytesort.hs
@@ -1,18 +1,17 @@
 -- See `bytesort.ts` for (pseudo)code
 module Data.Robustsort.Bytesort (bytesort, bytesort4Bit) where
 
-import Data.Robustsort.Subalgorithms.Bubblesort (bubblesort)
--- import Data.Robustsort.Utils.Bytes (convertRawBitsToBytes, getBytestacksFromBytes, getSortedArrayFromBytestacks, reduceBytestacks)
+import Data.Robustsort.Utils.Bytes (convertRawBitsToBytes, getBytestacksFromBytes, reduceBytestacks)
+import Data.Robustsort.Utils.Bytes2 (getSortedBitsFromBytestacks)
 import Data.Robustsort.Utils.RandomizeList (randomizeList)
 
 bytesort4Bit :: [Int] -> [Int]
 bytesort4Bit xs = bytesort xs 4
 
--- bytesort :: [Int] -> Int -> [Int]
+bytesort :: [Int] -> Int -> [Int]
 bytesort xs bytesize = do
-  -- let bits = randomizeList xs
-  -- let bytes = convertRawBitsToBytes bits bytesize
-  -- let bytestacks = getBytestacksFromBytes bytes bytesize
-  -- let bytestacks' = reduceBytestacks bytestacks bytesize
-  -- getSortedArrayFromBytestacks bytestacks'
-  xs
+  let bits = randomizeList xs
+  let bytes = convertRawBitsToBytes bits bytesize
+  let bytestacks = getBytestacksFromBytes bytes bytesize
+  let bytestacks' = reduceBytestacks bytestacks bytesize
+  getSortedBitsFromBytestacks bytestacks'
--- src/Data/Robustsort/Utils/Bytes2.hs
+++ src/Data/Robustsort/Utils/Bytes2.hs
@@ -1,5 +1,5 @@
 module Data.Robustsort.Utils.Bytes2
-  ( getSortedListFromBytestacks,
+  ( getSortedBitsFromBytestacks,
   )
 where
 
@@ -9,10 +9,10 @@ import Data.Robustsort.Utils.Types (Bytestack, Bytestore, Memory (..))
 -- | Compile a sorted list of Bits from a list of Bytestacks
 
 -- | ==== __Examples__
--- >>> getSortedListFromBytestacks [([(0,28),(1,38)],BigMemory [([(0,27),(1,28)],BigMemory [([(0,23),(1,27)],Memory [[21,23],[25,27]]),([(0,24),(1,28)],Memory [[22,24],[26,28]])]),([(1,37),(0,38)],BigMemory [([(0,33),(1,38)],Memory [[31,33],[35,38]]),([(0,34),(1,37)],Memory [[32,14],[36,37]])])])]
+-- >>> getSortedBitsFromBytestacks [([(0,28),(1,38)],BigMemory [([(0,27),(1,28)],BigMemory [([(0,23),(1,27)],Memory [[21,23],[25,27]]),([(0,24),(1,28)],Memory [[22,24],[26,28]])]),([(1,37),(0,38)],BigMemory [([(0,33),(1,38)],Memory [[31,33],[35,38]]),([(0,34),(1,37)],Memory [[32,14],[36,37]])])])]
 -- [14, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 35, 36, 37, 38]
-getSortedListFromBytestacks :: [Bytestack] -> [Int]
-getSortedListFromBytestacks bytestacksRaw = acc bytestacksRaw []
+getSortedBitsFromBytestacks :: [Bytestack] -> [Int]
+getSortedBitsFromBytestacks bytestacksRaw = acc bytestacksRaw []
   where
     acc :: [Bytestack] -> [Int] -> [Int]
     acc bytestacks sortedBits =
