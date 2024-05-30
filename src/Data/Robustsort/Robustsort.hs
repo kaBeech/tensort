04235b285d99c19a305d8387fcddb205c7541ab0
kaBeech <contact@kaBeech.com>
2024-05-29 17:34:47 -0700

    Add RobustsortB

--- src/Data/Robustsort/Robustsort.hs
+++ src/Data/Robustsort/Robustsort.hs
@@ -1,9 +1,14 @@
-module Data.Robustsort.Robustsort (robustsortP) where
+module Data.Robustsort.Robustsort
+  ( robustsortP,
+    robustsortB,
+  )
+where
 
 import Data.Robustsort.Bytesort (bytesort)
 import Data.Robustsort.Subalgorithms.Bogosort (bogosort)
 import Data.Robustsort.Subalgorithms.Bubblesort (bubblesort)
 import Data.Robustsort.Subalgorithms.Permutationsort (permutationsort)
+import Data.Robustsort.Subalgorithms.ReverseExchangesort (reverseExchangesort)
 import Data.Robustsort.Subalgorithms.Supersort (mundaneSuperStrat, supersort)
 import Data.Robustsort.Utils.Bytes (mkBSProps)
 import Data.Robustsort.Utils.Types (Sortable)
@@ -12,4 +17,10 @@ robustsortP :: Sortable -> Sortable
 robustsortP xs = bytesort xs (mkBSProps 3 supersortP)
 
 supersortP :: Sortable -> Sortable
-supersortP xs = supersort xs (bubblesort, bogosort, permutationsort, mundaneSuperStrat)
+supersortP xs = supersort xs (bubblesort, reverseExchangesort, permutationsort, mundaneSuperStrat)
+
+robustsortB :: Sortable -> Sortable
+robustsortB xs = bytesort xs (mkBSProps 3 supersortB)
+
+supersortB :: Sortable -> Sortable
+supersortB xs = supersort xs (bubblesort, reverseExchangesort, bogosort, mundaneSuperStrat)
