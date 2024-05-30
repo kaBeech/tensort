3dd88d5b7ee7abae84817e840cc39f8d1fb71644
kaBeech <contact@kaBeech.com>
2024-05-29 21:37:37 -0700

    Update Types for exports

--- src/Data/Tensort/Robustsort.hs
+++ src/Data/Tensort/Robustsort.hs
@@ -5,29 +5,29 @@ module Data.Tensort.Robustsort
   )
 where
 
-import Data.Tensort.Tensort (tensort)
 import Data.Tensort.Subalgorithms.Bogosort (bogosort)
 import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
 import Data.Tensort.Subalgorithms.Magicsort (magicsort)
 import Data.Tensort.Subalgorithms.Permutationsort (permutationsort)
 import Data.Tensort.Subalgorithms.ReverseExchangesort (reverseExchangesort)
 import Data.Tensort.Subalgorithms.Supersort (magicSuperStrat, mundaneSuperStrat, supersort)
+import Data.Tensort.Tensort (tensort)
 import Data.Tensort.Utils.Bytes (mkTSProps)
 import Data.Tensort.Utils.Types (Sortable)
 
-robustsortP :: Sortable -> Sortable
+robustsortP :: [Int] -> [Int]
 robustsortP xs = tensort xs (mkTSProps 3 supersortP)
 
 supersortP :: Sortable -> Sortable
 supersortP xs = supersort xs (bubblesort, reverseExchangesort, permutationsort, mundaneSuperStrat)
 
-robustsortB :: Sortable -> Sortable
+robustsortB :: [Int] -> [Int]
 robustsortB xs = tensort xs (mkTSProps 3 supersortB)
 
 supersortB :: Sortable -> Sortable
 supersortB xs = supersort xs (bubblesort, reverseExchangesort, bogosort, mundaneSuperStrat)
 
-robustsortM :: Sortable -> Sortable
+robustsortM :: [Int] -> [Int]
 robustsortM xs = tensort xs (mkTSProps 3 supersortM)
 
 supersortM :: Sortable -> Sortable
--- src/Data/Tensort/Tensort.hs
+++ src/Data/Tensort/Tensort.hs
@@ -11,13 +11,13 @@ import Data.Tensort.Utils.Bytes (TensortProps, convertRawBitsToBytes, getSortedB
 import Data.Tensort.Utils.RandomizeList (randomizeList)
 import Data.Tensort.Utils.Types (Sortable (..), fromSortInt)
 
-tensortBasic2Bit :: Sortable -> Sortable
+tensortBasic2Bit :: [Int] -> [Int]
 tensortBasic2Bit xs = tensort xs (mkTSProps 2 bubblesort)
 
-tensortBasic3Bit :: Sortable -> Sortable
+tensortBasic3Bit :: [Int] -> [Int]
 tensortBasic3Bit xs = tensort xs (mkTSProps 3 bubblesort)
 
-tensortBasic4Bit :: Sortable -> Sortable
+tensortBasic4Bit :: [Int] -> [Int]
 tensortBasic4Bit xs = tensort xs (mkTSProps 4 bubblesort)
 
 -- | Sort a list of Ints using the Tensort algorithm
@@ -31,10 +31,10 @@ tensortBasic4Bit xs = tensort xs (mkTSProps 4 bubblesort)
 -- [2,7,11,14,17,20,23,26,29,32,35,38,42,44,47,50,56,59,62,69]
 -- >>> tensort (randomizeList [1..100] 143) 2
 -- [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-tensort :: Sortable -> TensortProps -> Sortable
+tensort :: [Int] -> TensortProps -> [Int]
 tensort xs tsProps = do
-  let bits = randomizeList xs 143
+  let bits = randomizeList (SortInt xs) 143
   let bytes = convertRawBitsToBytes (fromSortInt bits) tsProps
   let tensorstacks = getTensorstacksFromBytes bytes tsProps
   let metastack = reduceTensorstacks tensorstacks tsProps
-  SortInt (getSortedBitsFromMetastack metastack tsProps)
+  getSortedBitsFromMetastack metastack tsProps
