072d8651c811dbce412e97426ca14283df3eb4ba
kaBeech <contact@kaBeech.com>
2024-06-12 19:17:02 -0700

    Cleanup

--- test/SortSpec.hs
+++ test/SortSpec.hs
@@ -2,7 +2,6 @@ module SortSpec
   ( result_is_sorted_bits,
     result_is_sorted_records,
     result_is_sorted_records_short,
-    result_is_sorted_sortable,
   )
 where
 
@@ -13,7 +12,7 @@ import Test.QuickCheck
 result_is_sorted_bits :: ([Bit] -> [Bit]) -> [Bit] -> Property
 result_is_sorted_bits sort unsortedList =
   within
-    1000000
+    100000
     ( (length unsortedList < 10) ==>
         isSorted (SortBit (sort unsortedList))
     )
@@ -21,7 +20,7 @@ result_is_sorted_bits sort unsortedList =
 result_is_sorted_records :: SortAlg -> [Record] -> Property
 result_is_sorted_records sort unsortedList =
   within
-    1000000
+    100000
     ( (length unsortedList < 10) ==>
         isSorted (sort (SortRec unsortedList))
     )
@@ -29,21 +28,7 @@ result_is_sorted_records sort unsortedList =
 result_is_sorted_records_short :: SortAlg -> [Record] -> Property
 result_is_sorted_records_short sort unsortedList =
   within
-    1000000
+    100000
     ( (length unsortedList < 6) ==>
-        isSorted (sort (SortRec unsortedList))
-    )
-
-result_is_sorted_sortable :: SortAlg -> Sortable -> Property
-result_is_sorted_sortable sort (SortBit unsortedList) =
-  within
-    1000000
-    ( (length unsortedList < 10) ==>
-        isSorted (sort (SortBit unsortedList))
-    )
-result_is_sorted_sortable sort (SortRec unsortedList) =
-  within
-    1000000
-    ( (length unsortedList < 10) ==>
         isSorted (sort (SortRec unsortedList))
     )
