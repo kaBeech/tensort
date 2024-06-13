64a808cdf0da06d990a2c1522a7c8183938be585
kaBeech <contact@kaBeech.com>
2024-06-12 18:59:42 -0700

    Remove not null guards

--- test/SortSpec.hs
+++ test/SortSpec.hs
@@ -5,14 +5,14 @@ import Data.Tensort.Utils.Types (Bit, Record, SortAlg, Sortable (..))
 import Test.QuickCheck
 
 result_is_sorted_bits :: ([Bit] -> [Bit]) -> [Bit] -> Property
-result_is_sorted_bits sort unsortedList = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (SortBit (sort unsortedList)))
+result_is_sorted_bits sort unsortedList = within 1000000 ((length unsortedList < 10) ==> isSorted (SortBit (sort unsortedList)))
 
 result_is_sorted_records :: SortAlg -> [Record] -> Property
-result_is_sorted_records sort unsortedList = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList)))
+result_is_sorted_records sort unsortedList = within 1000000 ((length unsortedList < 10) ==> isSorted (sort (SortRec unsortedList)))
 
 result_is_sorted_records_short :: SortAlg -> [Record] -> Property
-result_is_sorted_records_short sort unsortedList = within 1000000 ((length unsortedList < 6) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList)))
+result_is_sorted_records_short sort unsortedList = within 1000000 ((length unsortedList < 6) ==> isSorted (sort (SortRec unsortedList)))
 
 result_is_sorted_sortable :: SortAlg -> Sortable -> Property
-result_is_sorted_sortable sort (SortBit unsortedList) = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortBit unsortedList)))
-result_is_sorted_sortable sort (SortRec unsortedList) = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList)))
+result_is_sorted_sortable sort (SortBit unsortedList) = within 1000000 ((length unsortedList < 10) ==> isSorted (sort (SortBit unsortedList)))
+result_is_sorted_sortable sort (SortRec unsortedList) = within 1000000 ((length unsortedList < 10) ==> isSorted (sort (SortRec unsortedList)))
