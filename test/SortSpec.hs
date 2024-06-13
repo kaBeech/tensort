12a61611f03058ccb21466e85bbe4c3498544200
kaBeech <contact@kaBeech.com>
2024-06-12 18:59:16 -0700

    Add time limits to tests

--- test/SortSpec.hs
+++ test/SortSpec.hs
@@ -5,14 +5,14 @@ import Data.Tensort.Utils.Types (Bit, Record, SortAlg, Sortable (..))
 import Test.QuickCheck
 
 result_is_sorted_bits :: ([Bit] -> [Bit]) -> [Bit] -> Property
-result_is_sorted_bits sort unsortedList = (length unsortedList < 10) && not (null unsortedList) ==> isSorted (SortBit (sort unsortedList))
+result_is_sorted_bits sort unsortedList = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (SortBit (sort unsortedList)))
 
 result_is_sorted_records :: SortAlg -> [Record] -> Property
-result_is_sorted_records sort unsortedList = (length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList))
+result_is_sorted_records sort unsortedList = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList)))
 
 result_is_sorted_records_short :: SortAlg -> [Record] -> Property
 result_is_sorted_records_short sort unsortedList = within 1000000 ((length unsortedList < 6) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList)))
 
 result_is_sorted_sortable :: SortAlg -> Sortable -> Property
-result_is_sorted_sortable sort (SortBit unsortedList) = (length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortBit unsortedList))
-result_is_sorted_sortable sort (SortRec unsortedList) = (length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList))
+result_is_sorted_sortable sort (SortBit unsortedList) = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortBit unsortedList)))
+result_is_sorted_sortable sort (SortRec unsortedList) = within 1000000 ((length unsortedList < 10) && not (null unsortedList) ==> isSorted (sort (SortRec unsortedList)))
