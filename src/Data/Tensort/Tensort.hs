130a54995af52c522540213702f670593d8a44a1
kaBeech <contact@kaBeech.com>
2024-08-06 12:41:43 -0700

    Update doctest

--- src/Data/Tensort/Tensort.hs
+++ src/Data/Tensort/Tensort.hs
@@ -18,7 +18,10 @@ import Data.Tensort.Utils.Types (Bit, Sortable (..), TensortProps (..), fromSort
 -- | Sort a list of Bits using the Tensort algorithm
 
 -- | ==== __Examples__
--- >>> tensort (randomizeList [1..100] 143) 2
+-- >>> import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
+-- >>> import Data.Tensort.Utils.MkTsProps (mkTsProps)
+-- >>> let inputList = fromSortBit (randomizeList (SortBit [1..100]) 143)
+-- >>> tensort inputList (mkTsProps 2 bubblesort)
 -- [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
 tensort :: [Bit] -> TensortProps -> [Bit]
 tensort [] _ = []
