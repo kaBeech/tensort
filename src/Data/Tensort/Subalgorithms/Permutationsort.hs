cae793795c1ecb46c35ce540c4b0d67ffa83b02c
kaBeech <contact@kaBeech.com>
2024-08-07 11:37:24 -0700

    Add Wonkiness to Permutationsort

--- src/Data/Tensort/Subalgorithms/Permutationsort.hs
+++ src/Data/Tensort/Subalgorithms/Permutationsort.hs
@@ -2,22 +2,34 @@ module Data.Tensort.Subalgorithms.Permutationsort (permutationsort) where
 
 import Data.List (permutations)
 import Data.Tensort.Utils.Check (isSorted)
-import Data.Tensort.Utils.Types (Record, Sortable (..), fromSortBit, fromSortRec, Bit)
+import Data.Tensort.Utils.Types (Bit, Record, Sortable (..), WonkyState)
 
-permutationsort :: Sortable -> Sortable
-permutationsort (SortBit xs) = SortBit (acc (permutations x) [])
-  where
-    x = xs
-    acc :: [[Bit]] -> [Bit] -> [Bit]
-    acc [] unsortedPermutations = fromSortBit (permutationsort (SortBit unsortedPermutations))
-    acc (permutation : remainingPermutations) unsortedPermutations
-      | isSorted (SortBit permutation) = permutation
-      | otherwise = acc remainingPermutations unsortedPermutations
-permutationsort (SortRec xs) = SortRec (acc (permutations x) [])
-  where
-    x = xs
-    acc :: [[Record]] -> [Record] -> [Record]
-    acc [] unsortedPermutations = fromSortRec (permutationsort (SortRec unsortedPermutations))
-    acc (permutation : remainingPermutations) unsortedPermutations
-      | isSorted (SortRec permutation) = permutation
-      | otherwise = acc remainingPermutations unsortedPermutations
+permutationsort :: Sortable -> WonkyState -> (Sortable, WonkyState)
+permutationsort (SortBit bits) wonkySt = do
+  let initPermutations = permutations bits
+  let (result, wonkySt') = permutationsortBits initPermutations wonkySt
+  if null result
+    then (SortBit bits, wonkySt')
+    else (SortBit result, wonkySt')
+permutationsort (SortRec recs) wonkySt = do
+  let initPermutations = permutations recs
+  let (result, wonkySt') = permutationsortRecords initPermutations wonkySt
+  if null result
+    then (SortRec recs, wonkySt')
+    else (SortRec result, wonkySt')
+
+permutationsortBits :: [[Bit]] -> WonkyState -> ([Bit], WonkyState)
+permutationsortBits [] wonkySt = ([], wonkySt)
+permutationsortBits (bits : remainingPermutations) wonkySt = do
+  let (result, wonkySt') = isSorted (SortBit bits) wonkySt
+  if result
+    then (bits, wonkySt')
+    else permutationsortBits remainingPermutations wonkySt'
+
+permutationsortRecords :: [[Record]] -> WonkyState -> ([Record], WonkyState)
+permutationsortRecords [] wonkySt = ([], wonkySt)
+permutationsortRecords (recs : remainingPermutations) wonkySt = do
+  let (result, wonkySt') = isSorted (SortRec recs) wonkySt
+  if result
+    then (recs, wonkySt')
+    else permutationsortRecords remainingPermutations wonkySt'
