a576db5ab7ae0062251aa02e5917650a7ebd9aff
kaBeech <contact@kaBeech.com>
2024-08-07 13:24:47 -0700

    Add Wonkiness to Reduce

--- src/Data/Tensort/Utils/Reduce.hs
+++ src/Data/Tensort/Utils/Reduce.hs
@@ -2,7 +2,7 @@ module Data.Tensort.Utils.Reduce (reduceTensorStacks) where
 
 import Data.Tensort.Utils.Compose (createTensor)
 import Data.Tensort.Utils.Split (splitEvery)
-import Data.Tensort.Utils.Types (Memory (..), TensorStack, TensortProps (..))
+import Data.Tensort.Utils.Types (Memory (..), TensorStack, TensortProps (..), WonkyState)
 
 -- | Take a list of TensorStacks and group them together in new
 --   TensorStacks, each containing bytesize number of Tensors (former
@@ -15,12 +15,12 @@ import Data.Tensort.Utils.Types (Memory (..), TensorStack, TensortProps (..))
 -- >>> import Data.Tensort.Utils.MkTsProps (mkTsProps)
 -- >>> reduceTensorStacks [([(0, 33), (1, 38)], ByteMem [[31, 33], [35, 38]]), ([(0, 34), (1, 37)], ByteMem [[32, 14], [36, 37]]), ([(0, 23), (1, 27)], ByteMem [[21, 23], [25, 27]]), ([(0, 24), (1, 28)], ByteMem [[22, 24], [26, 28]]),([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]]),([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])] (mkTsProps 2 bubblesort)
 -- ([(1,18),(0,38)],TensorMem [([(0,28),(1,38)],TensorMem [([(0,27),(1,28)],TensorMem [([(0,23),(1,27)],ByteMem [[21,23],[25,27]]),([(0,24),(1,28)],ByteMem [[22,24],[26,28]])]),([(1,37),(0,38)],TensorMem [([(0,33),(1,38)],ByteMem [[31,33],[35,38]]),([(0,34),(1,37)],ByteMem [[32,14],[36,37]])])]),([(0,8),(1,18)],TensorMem [([(0,7),(1,8)],TensorMem [([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])]),([(1,17),(0,18)],TensorMem [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]])])])])
-reduceTensorStacks :: [TensorStack] -> TensortProps -> TensorStack
-reduceTensorStacks tensorStacks tsProps = do
-  let newTensorStacks = reduceTensorStacksSinglePass tensorStacks tsProps
+reduceTensorStacks :: [TensorStack] -> TensortProps -> WonkyState -> (TensorStack, WonkyState)
+reduceTensorStacks tensorStacks tsProps wonkySt = do
+  let (newTensorStacks, wonkySt') = reduceTensorStacksSinglePass tensorStacks tsProps wonkySt
   if length newTensorStacks <= bytesize tsProps
-    then createTensor (TensorMem newTensorStacks) (subAlgorithm tsProps)
-    else reduceTensorStacks newTensorStacks tsProps
+    then createTensor (TensorMem newTensorStacks) (subAlgorithm tsProps) wonkySt'
+    else reduceTensorStacks newTensorStacks tsProps wonkySt'
 
 -- | Take a list of TensorStacks  and group them together in new
 --   TensorStacks each containing bytesize number of Tensors (former TensorStacks)
@@ -32,8 +32,10 @@ reduceTensorStacks tensorStacks tsProps = do
 -- >>> import Data.Tensort.Utils.MkTsProps (mkTsProps)
 -- >>> reduceTensorStacksSinglePass [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]]),([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])] (mkTsProps 2 bubblesort)
 -- [([(0,7),(1,8)],TensorMem [([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])]),([(1,17),(0,18)],TensorMem [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]])])]
-reduceTensorStacksSinglePass :: [TensorStack] -> TensortProps -> [TensorStack]
-reduceTensorStacksSinglePass tensorStacks tsProps = foldr acc [] (splitEvery (bytesize tsProps) tensorStacks)
+reduceTensorStacksSinglePass :: [TensorStack] -> TensortProps -> WonkyState -> ([TensorStack], WonkyState)
+reduceTensorStacksSinglePass tensorStacks tsProps wonkySt = foldr acc ([], wonkySt) (splitEvery (bytesize tsProps) tensorStacks)
   where
-    acc :: [TensorStack] -> [TensorStack] -> [TensorStack]
-    acc tensorStack newTensorStacks = newTensorStacks ++ [createTensor (TensorMem tensorStack) (subAlgorithm tsProps)]
+    acc :: [TensorStack] -> ([TensorStack], WonkyState) -> ([TensorStack], WonkyState)
+    acc tensorStack (newTensorStacks, wonkySt') = do
+      let (tensor, wonkySt'') = createTensor (TensorMem tensorStack) (subAlgorithm tsProps) wonkySt'
+      (newTensorStacks ++ [tensor], wonkySt'')
