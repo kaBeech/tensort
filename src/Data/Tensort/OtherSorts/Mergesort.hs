fb8e43ef1d952a68520c18206bf456497e161692
kaBeech <contact@kaBeech.com>
2024-08-07 09:46:07 -0700

    Add Wonkiness to Mergesort

--- src/Data/Tensort/OtherSorts/Mergesort.hs
+++ src/Data/Tensort/OtherSorts/Mergesort.hs
@@ -1,44 +1,54 @@
 module Data.Tensort.OtherSorts.Mergesort (mergesort) where
 
 import Data.Tensort.Utils.ComparisonFunctions (lessThanBit, lessThanRecord)
-import Data.Tensort.Utils.Types (Record, Sortable (..), Bit)
+import Data.Tensort.Utils.Types (Bit, Record, Sortable (..), WonkyState)
 
-mergesort :: Sortable -> Sortable
-mergesort (SortBit xs) = SortBit (mergesortBits xs)
-mergesort (SortRec xs) = SortRec (mergesortRecs xs)
+mergesort :: Sortable -> WonkyState -> (Sortable, WonkyState)
+mergesort (SortBit xs) wonkySt = do
+  let (result, wonkySt') = mergesortBits xs wonkySt
+  (SortBit result, wonkySt')
+mergesort (SortRec xs) wonkySt = do
+  let (result, wonkySt') = mergesortRecs xs wonkySt
+  (SortRec result, wonkySt')
 
-mergesortBits :: [Bit] -> [Bit]
-mergesortBits = mergeAllBits . map (: [])
-  where
-    mergeAllBits [] = []
-    mergeAllBits [x] = x
-    mergeAllBits [x, y] = mergeBits x y
-    mergeAllBits remaningElements = mergeAllBits (mergePairs remaningElements)
+mergesortBits :: [Bit] -> WonkyState -> ([Bit], WonkyState)
+mergesortBits [] wonkySt = ([], wonkySt)
+mergesortBits [x] wonkySt = ([x], wonkySt)
+mergesortBits xs wonkySt = do
+  let (left, right) = splitAt (length xs `div` 2) xs
+  let (leftSorted, wonkySt') = mergesortBits left wonkySt
+  let (rightSorted, wonkySt'') = mergesortBits right wonkySt'
+  mergeBits leftSorted rightSorted wonkySt''
 
-    mergePairs (x : y : remaningElements) = mergeBits x y : mergePairs remaningElements
-    mergePairs x = x
+mergeBits :: [Bit] -> [Bit] -> WonkyState -> ([Bit], WonkyState)
+mergeBits [] y wonkySt = (y, wonkySt)
+mergeBits x [] wonkySt = (x, wonkySt)
+mergeBits (x : xs) (y : ys) wonkySt = do
+  let (firstElemLess, wonkySt') = lessThanBit x y wonkySt
+  if firstElemLess
+    then do
+      let (result, wonkySt'') = mergeBits xs (y : ys) wonkySt'
+      (x : result, wonkySt'')
+    else do
+      let (result, wonkySt'') = mergeBits (x : xs) ys wonkySt'
+      (y : result, wonkySt'')
 
-mergeBits :: [Bit] -> [Bit] -> [Bit]
-mergeBits [] y = y
-mergeBits x [] = x
-mergeBits (x : xs) (y : ys)
-  | lessThanBit x y = x : mergeBits xs (y : ys)
-  | otherwise = y : mergeBits (x : xs) ys
+mergesortRecs :: [Record] -> WonkyState -> ([Record], WonkyState)
+mergesortRecs recs wonkySt = do
+  let (left, right) = splitAt (length recs `div` 2) recs
+  let (leftSorted, wonkySt') = mergesortRecs left wonkySt
+  let (rightSorted, wonkySt'') = mergesortRecs right wonkySt'
+  mergeRecs leftSorted rightSorted wonkySt''
 
-mergesortRecs :: [Record] -> [Record]
-mergesortRecs = mergeAllRecs . map (: [])
-  where
-    mergeAllRecs [] = []
-    mergeAllRecs [x] = x
-    mergeAllRecs [x, y] = mergeRecs x y
-    mergeAllRecs remaningElements = mergeAllRecs (mergePairs remaningElements)
-
-    mergePairs (x : y : remaningElements) = mergeRecs x y : mergePairs remaningElements
-    mergePairs x = x
-
-mergeRecs :: [Record] -> [Record] -> [Record]
-mergeRecs [] y = y
-mergeRecs x [] = x
-mergeRecs (x : xs) (y : ys)
-  | lessThanRecord x y = x : mergeRecs xs (y : ys)
-  | otherwise = y : mergeRecs (x : xs) ys
+mergeRecs :: [Record] -> [Record] -> WonkyState -> ([Record], WonkyState)
+mergeRecs [] y wonkySt = (y, wonkySt)
+mergeRecs x [] wonkySt = (x, wonkySt)
+mergeRecs (x : xs) (y : ys) wonkySt = do
+  let (firstElemLess, wonkySt') = lessThanRecord x y wonkySt
+  if firstElemLess
+    then do
+      let (result, wonkySt'') = mergeRecs xs (y : ys) wonkySt'
+      (x : result, wonkySt'')
+    else do
+      let (result, wonkySt'') = mergeRecs (x : xs) ys wonkySt'
+      (y : result, wonkySt'')
