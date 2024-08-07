3bb0a4611fae973880203657d525798cc04985db
kaBeech <contact@kaBeech.com>
2024-08-07 14:20:19 -0700

    Add Wonkiness to Tensort

--- src/Data/Tensort/Tensort.hs
+++ src/Data/Tensort/Tensort.hs
@@ -13,7 +13,7 @@ import Data.Tensort.Utils.MkTsProps (mkTsProps)
 import Data.Tensort.Utils.RandomizeList (randomizeList)
 import Data.Tensort.Utils.Reduce (reduceTensorStacks)
 import Data.Tensort.Utils.Render (getSortedBitsFromTensor)
-import Data.Tensort.Utils.Types (Bit, Sortable (..), TensortProps (..), fromSortBit)
+import Data.Tensort.Utils.Types (Bit, Sortable (..), TensortProps (..), fromSortBit, WonkyState)
 
 -- | Sort a list of Bits using the Tensort algorithm
 
@@ -23,26 +23,26 @@ import Data.Tensort.Utils.Types (Bit, Sortable (..), TensortProps (..), fromSort
 -- >>> let inputList = fromSortBit (randomizeList (SortBit [1..100]) 143)
 -- >>> tensort inputList (mkTsProps 2 bubblesort)
 -- [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-tensort :: [Bit] -> TensortProps -> [Bit]
-tensort [] _ = []
-tensort xs tsProps = do
+tensort :: [Bit] -> TensortProps -> WonkyState -> ([Bit], WonkyState)
+tensort [] _ wonkySt = ([], wonkySt)
+tensort xs tsProps wonkySt = do
   let bits = randomizeList (SortBit xs) 143
-  let bytes = rawBitsToBytes (fromSortBit bits) tsProps
-  let tensorStacks = createInitialTensors bytes tsProps
-  let topTensor = reduceTensorStacks tensorStacks tsProps
-  getSortedBitsFromTensor topTensor (subAlgorithm tsProps)
+  let (bytes, wonkySt') = rawBitsToBytes (fromSortBit bits) tsProps wonkySt
+  let (tensorStacks, wonkySt'') = createInitialTensors bytes tsProps wonkySt'
+  let (topTensor, wonkySt''') = reduceTensorStacks tensorStacks tsProps wonkySt''
+  getSortedBitsFromTensor topTensor (subAlgorithm tsProps) wonkySt'''
 
-tensortB4 :: [Bit] -> [Bit]
-tensortB4 xs = tensort xs (mkTsProps 4 bubblesort)
+tensortB4 :: [Bit] -> WonkyState -> ([Bit], WonkyState)
+tensortB4 xs wonkySt = tensort xs (mkTsProps 4 bubblesort) wonkySt
 
-tensortBN :: Int -> [Bit] -> [Bit]
-tensortBN n xs = tensort xs (mkTsProps n bubblesort)
+tensortBN :: Int -> [Bit] -> WonkyState -> ([Bit], WonkyState)
+tensortBN n xs wonkySt = tensort xs (mkTsProps n bubblesort) wonkySt
 
-tensortBL :: [Bit] -> [Bit]
-tensortBL [] = []
-tensortBL [x] = [x]
-tensortBL [x, y] = if x <= y then [x, y] else [y, x]
-tensortBL xs = tensort xs (mkTsProps (calculateBytesize xs) bubblesort)
+tensortBL :: [Bit] -> WonkyState -> ([Bit], WonkyState)
+tensortBL [] wonkySt = ([], wonkySt)
+tensortBL [x] wonkySt = ([x], wonkySt)
+tensortBL [x, y] wonkySt = if x <= y then ([x, y], wonkySt) else ([y, x], wonkySt)
+tensortBL xs wonkySt = tensort xs (mkTsProps (calculateBytesize xs) bubblesort) wonkySt
 
 calculateBytesize :: [Bit] -> Int
 calculateBytesize xs = ceiling (log (fromIntegral (length xs)) :: Double)
