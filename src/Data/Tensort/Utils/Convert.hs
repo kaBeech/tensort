c88de5b9419a12231e9e1ea8628d1b5b0e9ba042
kaBeech <contact@kaBeech.com>
2024-08-07 14:09:58 -0700

    Add Wonkiness to Convert

--- src/Data/Tensort/Utils/Convert.hs
+++ src/Data/Tensort/Utils/Convert.hs
@@ -1,7 +1,7 @@
 module Data.Tensort.Utils.Convert (rawBitsToBytes) where
 
 import Data.Tensort.Utils.Split (splitEvery)
-import Data.Tensort.Utils.Types (Bit, Byte, Sortable (..), TensortProps (..), fromSortBit)
+import Data.Tensort.Utils.Types (Bit, Byte, Sortable (..), TensortProps (..), fromSortBit, WonkyState)
 
 -- | Convert a list of Bits to a list of Bytes of given bytesize, sorting
 --   each byte with the given subalgorithm.
@@ -11,8 +11,10 @@ import Data.Tensort.Utils.Types (Bit, Byte, Sortable (..), TensortProps (..), fr
 --   >>> import Data.Tensort.Utils.MkTsProps (mkTsProps)
 --   >>> rawBitsToBytes [5,1,3,7,8,2,4,6] (mkTsProps 4 bubblesort)
 --   [[2,4,6,8],[1,3,5,7]]
-rawBitsToBytes :: [Bit] -> TensortProps -> [Byte]
-rawBitsToBytes bits tsProps = foldr acc [] (splitEvery (bytesize tsProps) bits)
+rawBitsToBytes :: [Bit] -> TensortProps -> WonkyState -> ([Byte], WonkyState)
+rawBitsToBytes bits tsProps wonkySt = foldr acc ([], wonkySt) (splitEvery (bytesize tsProps) bits)
   where
-    acc :: [Bit] -> [Byte] -> [Byte]
-    acc byte bytes = bytes ++ [fromSortBit (subAlgorithm tsProps (SortBit byte))]
+    acc :: [Bit] -> ([Byte], WonkyState) -> ([Byte], WonkyState)
+    acc byte (bytes, wonkySt') = do
+      let (result, wonkySt'') = (subAlgorithm tsProps) (SortBit byte) wonkySt'
+      ((bytes ++ [fromSortBit result]), wonkySt'')
