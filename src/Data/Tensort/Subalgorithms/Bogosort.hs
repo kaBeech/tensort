60b38c1b7c70b046e7fad7dbb530dba7fb21ffbd
kaBeech <contact@kaBeech.com>
2024-08-07 11:04:40 -0700

    Add Wonkiness to Bogosort

--- src/Data/Tensort/Subalgorithms/Bogosort.hs
+++ src/Data/Tensort/Subalgorithms/Bogosort.hs
@@ -2,12 +2,14 @@ module Data.Tensort.Subalgorithms.Bogosort (bogosort, bogosortSeeded) where
 
 import Data.Tensort.Utils.Check (isSorted)
 import Data.Tensort.Utils.RandomizeList (randomizeList)
-import Data.Tensort.Utils.Types (Sortable (..))
+import Data.Tensort.Utils.Types (Sortable (..), WonkyState)
 
-bogosort :: Sortable -> Sortable
+bogosort :: Sortable -> WonkyState -> (Sortable, WonkyState)
 bogosort xs = bogosortSeeded xs 143
 
-bogosortSeeded :: Sortable -> Int -> Sortable
-bogosortSeeded xs seed
-  | isSorted xs = xs
-  | otherwise = bogosortSeeded (randomizeList xs seed) (seed + 1)
+bogosortSeeded :: Sortable -> Int -> WonkyState -> (Sortable, WonkyState)
+bogosortSeeded xs seed wonkySt = do
+  let (result, wonkySt') = isSorted xs wonkySt
+  if result
+    then (xs, wonkySt')
+    else bogosortSeeded (randomizeList xs seed) (seed + 1) wonkySt'
