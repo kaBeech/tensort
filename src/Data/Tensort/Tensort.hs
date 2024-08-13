e9b3f3c61f6e5cecd36dc79c2f4f3cb66d79b57e
kaBeech <contact@kaBeech.com>
2024-08-13 10:02:36 -0700

    Pattern match for small input lists in base Tensort

--- src/Data/Tensort/Tensort.hs
+++ src/Data/Tensort/Tensort.hs
@@ -25,6 +25,8 @@ import Data.Tensort.Utils.Types (Sortable (..), TensortProps (..), fromSBitBits,
 -- [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
 tensort :: TensortProps -> Sortable -> Sortable
 tensort _ (SortBit []) = SortBit []
+tensort _ (SortBit [x]) = SortBit [x]
+tensort tsProps (SortBit [x, y]) = subAlgorithm tsProps (SortBit [x, y])
 tensort tsProps (SortBit xs) = do
   let bits = randomizeList 143 (SortBit xs)
   let bytes = rawToBytes tsProps bits
@@ -32,6 +34,8 @@ tensort tsProps (SortBit xs) = do
   let topTensor = reduceTensorStacks tsProps tensorStacks
   fromSBitBits (getSortedBitsFromTensor (subAlgorithm tsProps) topTensor)
 tensort _ (SortRec []) = SortRec []
+tensort _ (SortRec [x]) = SortRec [x]
+tensort tsProps (SortRec [x, y]) = subAlgorithm tsProps (SortRec [x, y])
 tensort tsProps (SortRec xs) = do
   let recs = randomizeList 143 (SortRec xs)
   let bytes = rawToBytes tsProps recs
@@ -46,9 +50,6 @@ tensortBN :: Int -> Sortable -> Sortable
 tensortBN n = tensort (mkTsProps n bubblesort)
 
 tensortBL :: Sortable -> Sortable
-tensortBL (SortBit []) = SortBit []
-tensortBL (SortBit [x]) = SortBit [x]
-tensortBL (SortBit [x, y]) = if x <= y then SortBit [x, y] else SortBit [y, x]
 tensortBL xs = tensort (mkTsProps (calculateBytesize xs) bubblesort) xs
 
 calculateBytesize :: Sortable -> Int
