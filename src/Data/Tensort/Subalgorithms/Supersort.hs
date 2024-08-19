1bc48f7a20dd90cb593f0a4d1a6162a88c8da90a
kaBeech <contact@kaBeech.com>
2024-08-19 16:59:02 -0700

    Simplify

--- src/Data/Tensort/Subalgorithms/Supersort.hs
+++ src/Data/Tensort/Subalgorithms/Supersort.hs
@@ -16,27 +16,15 @@ supersort ::
   (SortAlg, SortAlg, SortAlg, SupersortStrat) ->
   Sortable ->
   Sortable
-supersort (subAlg1, subAlg2, subAlg3, superStrat) (SortBit xs) = do
-  let result1 = subAlg1 (SortBit xs)
-  let result2 = subAlg2 (SortBit xs)
+supersort (subAlg1, subAlg2, subAlg3, superStrat) xs = do
+  let result1 = subAlg1 xs
+  let result2 = subAlg2 xs
   if result1 == result2
     then result1
-    else superStrat (result1, result2, subAlg3 (SortBit xs))
-supersort (subAlg1, subAlg2, subAlg3, superStrat) (SortRec xs) = do
-  let result1 = subAlg1 (SortRec xs)
-  let result2 = subAlg2 (SortRec xs)
-  if result1 == result2
-    then result1
-    else superStrat (result1, result2, subAlg3 (SortRec xs))
+    else superStrat (result1, result2, subAlg3 xs)
 
 mundaneSuperStrat :: SupersortStrat
-mundaneSuperStrat (SortBit result1, SortBit result2, SortBit result3) = do
-  if result2 == result3 then SortBit result2 else SortBit result1
-mundaneSuperStrat (SortRec result1, SortRec result2, SortRec result3) = do
-  if result2 == result3 then SortRec result2 else SortRec result1
-mundaneSuperStrat (_, _, _) =
-  error
-    "From Mundane SuperStrat: All three inputs must be of the same type."
+mundaneSuperStrat (result1, result2, result3) = if result2 == result3 then result2 else result1
 
 -- Previously we used different SuperStrats for Mundane and Magic Supersorts.
 -- Currently there is no need to differentiate, but we keep this here in case
