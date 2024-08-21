292b03b62b13dc119d4cd04dd924529120f6da16
kaBeech <contact@kaBeech.com>
2024-08-20 17:38:18 -0700

    Add default greeting

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,36 @@ module Main where
 import Benchmarking.MakeDeckShuffleChart (makeDeckShuffleChart)
 import Benchmarking.PrintDeckShuffleErrors (printDeckShuffleErrors)
 import Benchmarking.PrintErrorSpread (printErrorSpread)
-import Benchmarking.PrintLargeTimeAndErrorRateComparison (printLargeTimeAndErrorRateComparison)
-import Benchmarking.PrintSubAlgErrorRateComparison (printSubAlgErrorRateComparison)
+import Benchmarking.PrintLargeTimeAndErrorRateComparison
+  ( printLargeTimeAndErrorRateComparison,
+  )
+import Benchmarking.PrintSubAlgErrorRateComparison
+  ( printSubAlgErrorRateComparison,
+  )
 
 wonkyChance :: Int
 wonkyChance = 10
 
 stuckChance :: Int
-stuckChance = 50
+stuckChance = 0
 
 main :: IO ()
 main = do
+  -- makeDeckShuffleChart
   -- printSubAlgErrorRateComparison 1000 wonkyChance stuckChance
   -- printErrorSpread 1000 wonkyChance stuckChance
   -- printDeckShuffleErrors 1000 wonkyChance stuckChance
-  -- makeDeckShuffleChart
-  printLargeTimeAndErrorRateComparison [3 .. 14] 100 2048 wonkyChance stuckChance
+  -- printLargeTimeAndErrorRateComparison
+  --   [3 .. 14]
+  --   100
+  --   2048
+  --   wonkyChance
+  --   stuckChance
+  printGreeting
+
+printGreeting :: IO ()
+printGreeting =
+  print
+    "Welcome! To run the benchmarks, please uncomment the \
+    \desired function in `app/Main.hs`, tweak any settings desired, and run \
+    \the program."
