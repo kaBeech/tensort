ee40778a9fa0e6b802019859ace50c5f4aeb3ea1
kaBeech <contact@kaBeech.com>
2024-08-21 19:45:30 -0700

    Add Supersort module documentation

--- src/Data/Tensort/Subalgorithms/Supersort.hs
+++ src/Data/Tensort/Subalgorithms/Supersort.hs
@@ -1,3 +1,5 @@
+-- | This module provides functions for creating Supersort variants for
+--   adjudicating between 3 sorting algorithms that use the Sortable type
 module Data.Tensort.Subalgorithms.Supersort
   ( supersort,
     mundaneSuperStrat,
@@ -11,6 +13,19 @@ import Data.Tensort.Utils.Types
     SupersortStrat,
   )
 
+-- | Takes 3 sorting algorithms and a SuperStrat and returns a SortAlg that
+--   adjudicates between the 3 sorting algorithms using the provided SuperStrat
+
+-- | ==== __Examples__
+-- >>> import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
+-- >>> import Data.Tensort.Subalgorithms.Permutationsort (permutationsort)
+-- >>> import Data.Tensort.OtherSorts.Mergesort (mergesort)
+--
+-- >>> supersort (mergesort, bubblesort, permutationsort, mundaneSuperStrat) (SortBit [16, 23, 4, 8, 15, 42])
+-- SortBit [4,8,15,16,23,42]
+--
+-- >>> supersort (mergesort, bubblesort, permutationsort, mundaneSuperStrat) (SortRec [(16, 23), (4, 8), (15, 42)])
+-- SortRec [(4,8),(16,23),(15,42)]
 supersort ::
   (SortAlg, SortAlg, SortAlg, SupersortStrat) ->
   Sortable ->
@@ -22,11 +37,38 @@ supersort (subAlg1, subAlg2, subAlg3, superStrat) xs = do
     then result1
     else superStrat (result1, result2, subAlg3 xs)
 
+-- | Takes 3 SortAlgs and adjudicates between them to find a common result to
+--   increase robustness
+
+-- | ==== __Examples__
+-- >>> import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
+-- >>> import Data.Tensort.OtherSorts.Mergesort (mergesort)
+-- >>> import Data.Tensort.Subalgorithms.Permutationsort (permutationsort)
+--
+-- >>> supersort (mergesort, bubblesort, permutationsort, mundaneSuperStrat) (SortBit [16, 23, 4, 8, 15, 42])
+-- SortBit [4,8,15,16,23,42]
+--
+-- >>> supersort (mergesort, bubblesort, permutationsort, mundaneSuperStrat) (SortRec [(16, 23), (4, 8), (15, 42)])
+-- SortRec [(4,8),(16,23),(15,42)]
 mundaneSuperStrat :: SupersortStrat
 mundaneSuperStrat (result1, result2, result3) = if result2 == result3 then result2 else result1
 
--- Previously we used different SuperStrats for Mundane and Magic Supersorts.
--- Currently there is no need to differentiate, but we keep this here for
--- backwards compatibility and in case this changes again in the future
+-- | Takes 3 SortAlgs and adjudicates between them to find a common result to
+--   increase robustness
+--
+--   Previously we used different SuperStrats for Mundane and Magic Supersorts.
+--   Currently there is no need to differentiate, but we keep this here for
+--   backwards compatibility and in case this changes again in the future
+
+-- | ==== __Examples__
+-- >>> import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
+-- >>> import Data.Tensort.OtherSorts.Mergesort (mergesort)
+-- >>> import Data.Tensort.Subalgorithms.Permutationsort (permutationsort)
+--
+-- >>> supersort (mergesort, bubblesort, permutationsort, magicSuperStrat) (SortBit [16, 23, 4, 8, 15, 42])
+-- SortBit [4,8,15,16,23,42]
+--
+-- >>> supersort (mergesort, bubblesort, permutationsort, magicSuperStrat) (SortRec [(16, 23), (4, 8), (15, 42)])
+-- SortRec [(4,8),(16,23),(15,42)]
 magicSuperStrat :: SupersortStrat
 magicSuperStrat = mundaneSuperStrat
