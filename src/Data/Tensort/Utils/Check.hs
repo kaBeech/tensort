da5e4cf32196101b59ccc8b89e99f94811acbb04
kaBeech <contact@kaBeech.com>
2024-08-21 19:51:25 -0700

    Add Check module documentation

--- src/Data/Tensort/Utils/Check.hs
+++ src/Data/Tensort/Utils/Check.hs
@@ -1,8 +1,19 @@
+-- | This module provides the isSorted function, which checks if a list of
+--   elements is sorted in ascending order.
 module Data.Tensort.Utils.Check (isSorted) where
 
 import Data.Tensort.Utils.ComparisonFunctions (lessThanOrEqualBit, lessThanOrEqualRecord)
 import Data.Tensort.Utils.Types (Sortable (..))
 
+-- | Takes a Sortable list and returns True if the list is sorted in ascending
+--   order and False otherwise.
+
+-- | ==== __Examples__
+-- >>> isSorted (SortBit [0, 1, 2, 3, 4])
+-- True
+--
+-- >>> isSorted (SortBit [0, 1, 2, 4, 3])
+-- False
 isSorted :: Sortable -> Bool
 isSorted (SortBit []) = True
 isSorted (SortBit [_]) = True
