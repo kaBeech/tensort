faccfa1a2e3a29d3b5ded95fa3d237704f09b18e
kaBeech <contact@kaBeech.com>
2024-09-16 13:57:45 -0700

    Cleanup

--- src/Data/Tensort/Subalgorithms/Bogosort.hs
+++ src/Data/Tensort/Subalgorithms/Bogosort.hs
@@ -6,8 +6,7 @@ import Data.Tensort.Utils.Check (isSorted)
 import Data.Tensort.Utils.RandomizeList (randomizeList)
 import Data.Tensort.Utils.Types (Sortable (..))
 
--- | Takes a Sortable and returns a sorted Sortable using a Bogosort algorithm
---   using the default seed for random generation
+-- | Takes a Sortable and returns a sorted Sortable using a Bogosort algorithm.
 
 -- | ==== __Examples__
 -- >>> bogosort (SortBit [16, 23, 4, 8, 15, 42])
@@ -19,13 +18,13 @@ bogosort :: Sortable -> Sortable
 bogosort = bogosortSeeded 143
 
 -- | Takes a seed for use in random generation and a Sortable and returns a
---  sorted Sortable using a Bogosort algorithm
+--  sorted Sortable using a Bogosort algorithm.
 
 -- | ==== __Examples__
 -- >>> bogosortSeeded 42 (SortBit [16, 23, 4, 8, 15, 42])
 -- SortBit [4,8,15,16,23,42]
 --
--- >>> bogosortSeeded 24 (SortRec [(1, 16), (5, 23), (2, 4) ,(3, 8), (0, 15) , (4, 42)])
+-- >>> bogosortSeeded 24 (SortRec [(1, 16), (5, 23), (2, 4), (3, 8), (0, 15), (4, 42)])
 -- SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
 bogosortSeeded :: Int -> Sortable -> Sortable
 bogosortSeeded seed xs
