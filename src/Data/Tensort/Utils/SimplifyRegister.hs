ccc40cefb0eb4bb0108893cc25553925e34183ac
kaBeech <contact@kaBeech.com>
2024-09-16 14:43:34 -0700

    Add documentation

--- src/Data/Tensort/Utils/SimplifyRegister.hs
+++ src/Data/Tensort/Utils/SimplifyRegister.hs
@@ -1,3 +1,5 @@
+-- | This module provides functions to simplify sorting Registers in Tensort
+--   variants that are sorting Records in their input instead of Bits.
 module Data.Tensort.Utils.SimplifyRegister
   ( simplifyRegister,
     applySortingFromSimplifiedRegister,
@@ -7,9 +9,22 @@ where
 import qualified Data.Bifunctor
 import Data.Tensort.Utils.Types (Record, RecordR)
 
+-- | For use in Tensort variants that are sorting Records in their input
+--   instead of Bits.
+--
+--   This function simplifies the Register used for sorting Records by
+--   replacing the TopRecord of each RecordR with its TopBit. This returns
+--   a Register of standard Records that can be used for sorting.
 simplifyRegister :: [RecordR] -> [Record]
 simplifyRegister = map (Data.Bifunctor.second snd)
 
+-- | For use in Tensort variants that are sorting Records in their input
+--  instead of Bits.
+--
+--  This function takes a simplified Register that has been sorted and the
+--  unsimplified, unsorted, original RegisterR. It puts the elements of the
+--  original RegisterR in the same order as the simplified, sorted Register
+--  and returns the sorted RegisterR.
 applySortingFromSimplifiedRegister :: [Record] -> [RecordR] -> [RecordR]
 applySortingFromSimplifiedRegister
   sortedSimplifiedRegister
