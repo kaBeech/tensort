8904ac5b4d7f78227733548914487afad8d1ae85
kaBeech <contact@kaBeech.com>
2025-01-26 15:15:10 -0800

    Improve function readability

--- src/Data/Tensort/Utils/RandomizeList.hs
+++ src/Data/Tensort/Utils/RandomizeList.hs
@@ -17,5 +17,5 @@ import System.Random.Shuffle (shuffle')
 -- >>> randomizeList 143 (SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)])
 -- SortRec [(1,16),(5,23),(2,4),(3,8),(0,15),(4,42)]
 randomizeList :: Int -> Sortable -> Sortable
-randomizeList seed (SortBit xs) = SortBit (shuffle' xs (length xs) (mkStdGen seed))
-randomizeList seed (SortRec xs) = SortRec (shuffle' xs (length xs) (mkStdGen seed))
+randomizeList seed (SortBit xs) = SortBit $ shuffle' xs (length xs) $ mkStdGen seed
+randomizeList seed (SortRec xs) = SortRec $ shuffle' xs (length xs) $ mkStdGen seed
