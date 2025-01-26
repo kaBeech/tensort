98795582c64ab86220cc9c4d3c14011e351932ba
kaBeech <contact@kaBeech.com>
2025-01-26 13:35:25 -0800

    Improve function readability

--- src/Data/Tensort/Subalgorithms/Exchangesort.hs
+++ src/Data/Tensort/Subalgorithms/Exchangesort.hs
@@ -14,8 +14,8 @@ import Data.Tensort.Utils.Types (Sortable (..))
 -- >>> exchangesort (SortRec [(1, 16), (5, 23), (2, 4) ,(3, 8), (0, 15) , (4, 42)])
 -- SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
 exchangesort :: Sortable -> Sortable
-exchangesort (SortBit bits) = SortBit (exchangesortIterable greaterThanBit bits 0 (length bits - 1))
-exchangesort (SortRec recs) = SortRec (exchangesortIterable greaterThanRecord recs 0 (length recs - 1))
+exchangesort (SortBit bits) = SortBit $ exchangesortIterable greaterThanBit bits 0 (length bits - 1)
+exchangesort (SortRec recs) = SortRec $ exchangesortIterable greaterThanRecord recs 0 (length recs - 1)
 
 exchangesortIterable :: (Ord a) => (a -> a -> Bool) -> [a] -> Int -> Int -> [a]
 exchangesortIterable greaterThan xs i j
@@ -26,15 +26,16 @@ exchangesortIterable greaterThan xs i j
   | i == j =
       exchangesortIterable greaterThan xs i (j - 1)
   | otherwise =
-      let mini = min i j
-          maxi = max i j
-          left = take mini xs
-          middle = take (maxi - mini - 1) (drop (mini + 1) xs)
-          right = drop (maxi + 1) xs
-          x = xs !! mini
-          y = xs !! maxi
-          leftElemGreater = greaterThan x y
-          swappedXs = left ++ [y] ++ middle ++ [x] ++ right
-       in if leftElemGreater
-            then exchangesortIterable greaterThan swappedXs i (j - 1)
-            else exchangesortIterable greaterThan xs i (j - 1)
+      if leftElemGreater
+        then exchangesortIterable greaterThan swappedXs i (j - 1)
+        else exchangesortIterable greaterThan xs i (j - 1)
+  where
+    mini = min i j
+    maxi = max i j
+    left = take mini xs
+    middle = take (maxi - mini - 1) (drop (mini + 1) xs)
+    right = drop (maxi + 1) xs
+    x = xs !! mini
+    y = xs !! maxi
+    leftElemGreater = greaterThan x y
+    swappedXs = left ++ [y] ++ middle ++ [x] ++ right
