3633efd24940b286ba51be642fc91da0b9ba5749
kaBeech <contact@kaBeech.com>
2025-01-26 16:32:30 -0800

    Improve function readability

--- test/Test/SortSpec.hs
+++ test/Test/SortSpec.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use camelCase" #-}
 module Test.SortSpec
   ( result_is_sorted_bits,
     result_is_sorted_bits_short,
@@ -18,73 +21,66 @@ import Test.QuickCheck
 
 result_is_sorted_bits :: SortAlg -> [Bit] -> Property
 result_is_sorted_bits sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 10)
-        ==> isSorted (sort (SortBit unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 10
+    success = isSorted . sort $ SortBit unsortedList
 
 result_is_sorted_bits_short :: SortAlg -> [Bit] -> Property
 result_is_sorted_bits_short sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 6)
-        ==> isSorted (sort (SortBit unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 6
+    success = isSorted . sort $ SortBit unsortedList
 
 result_is_sorted_bits_tiny :: SortAlg -> [Bit] -> Property
 result_is_sorted_bits_tiny sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 3)
-        ==> isSorted (sort (SortBit unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 3
+    success = isSorted . sort $ SortBit unsortedList
 
 result_is_sorted_records :: SortAlg -> [Record] -> Property
 result_is_sorted_records sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 10)
-        ==> isSorted (sort (SortRec unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 10
+    success = isSorted . sort $ SortRec unsortedList
 
 result_is_sorted_records_short :: SortAlg -> [Record] -> Property
 result_is_sorted_records_short sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 6)
-        ==> isSorted (sort (SortRec unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 6
+    success = isSorted . sort $ SortRec unsortedList
 
 result_is_sorted_records_tiny :: SortAlg -> [Record] -> Property
 result_is_sorted_records_tiny sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 3)
-        ==> isSorted (sort (SortRec unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 3
+    success = isSorted . sort $ SortRec unsortedList
 
 result_is_sorted_custom_bitsize :: Int -> [Bit] -> Property
 result_is_sorted_custom_bitsize n unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 15)
-        && (n > 1)
-          ==> isSorted (tensortBN n (SortBit unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = (length unsortedList < 15) && (n > 1)
+    success = isSorted (tensortBN n (SortBit unsortedList))
 
 result_is_sorted_bits_only :: ([Bit] -> [Bit]) -> [Bit] -> Property
 result_is_sorted_bits_only sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 10)
-        ==> isSorted (SortBit (sort unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 10
+    success = isSorted . SortBit $ sort unsortedList
 
 result_is_sorted_bits_only_short :: ([Bit] -> [Bit]) -> [Bit] -> Property
 result_is_sorted_bits_only_short sort unsortedList =
-  within
-    1000000
-    ( (length unsortedList < 6)
-        ==> isSorted (SortBit (sort unsortedList))
-    )
+  within limitStd (constraint ==> success)
+  where
+    constraint = length unsortedList < 6
+    success = isSorted . SortBit $ sort unsortedList
+
+limitStd :: Int
+limitStd = 1000000
