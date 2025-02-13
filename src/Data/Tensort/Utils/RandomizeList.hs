c1f3c015bf16e7dddea0cc002bafac88bca22a08
kaBeech <contact@kaBeech.com>
2025-02-12 18:34:19 -0800

    Simplify Bit and Record sorting in RandomizeList

--- src/Data/Tensort/Utils/RandomizeList.hs
+++ src/Data/Tensort/Utils/RandomizeList.hs
@@ -1,14 +1,12 @@
 -- | This module prvodies the randomizeList function, which randomizes the
---   order of elements in Sortable lists.
+--   order of elements in sortable lists.
 module Data.Tensort.Utils.RandomizeList (randomizeList) where
 
-import Data.Tensort.Utils.Types (Sortable (..))
 import System.Random (mkStdGen)
 import System.Random.Shuffle (shuffle')
 
--- | Takes a seed for random generation and a Sortable list and returns a new
---   Sortable list with the same elements as the input list but in a random
---   order.
+-- | Takes a seed for random generation and a list and returns a new
+--   list with the same elements as the input list but in a random order.
 
 -- | ==== __Examples__
 -- >>> randomizeList 143 (SortBit [4, 8, 15, 16, 23, 42])
@@ -16,6 +14,5 @@ import System.Random.Shuffle (shuffle')
 --
 -- >>> randomizeList 143 (SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)])
 -- SortRec [(1,16),(5,23),(2,4),(3,8),(0,15),(4,42)]
-randomizeList :: Int -> Sortable -> Sortable
-randomizeList seed (SortBit xs) = SortBit $ shuffle' xs (length xs) $ mkStdGen seed
-randomizeList seed (SortRec xs) = SortRec $ shuffle' xs (length xs) $ mkStdGen seed
+randomizeList :: (Ord a) => Int -> [a] -> [a]
+randomizeList seed xs = shuffle' xs (length xs) $ mkStdGen seed
