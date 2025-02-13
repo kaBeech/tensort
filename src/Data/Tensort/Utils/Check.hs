367240d42ee877bb3df3c5cddde0516c3c4adc48
kaBeech <contact@kaBeech.com>
2025-02-12 16:36:50 -0800

    Simplify Bit and Record sorting in isSorted

--- src/Data/Tensort/Utils/Check.hs
+++ src/Data/Tensort/Utils/Check.hs
@@ -2,12 +2,6 @@
 --   elements is sorted in ascending order.
 module Data.Tensort.Utils.Check (isSorted) where
 
-import Data.Tensort.Utils.ComparisonFunctions
-  ( lessThanOrEqualBit,
-    lessThanOrEqualRecord,
-  )
-import Data.Tensort.Utils.Types (Sortable (..))
-
 -- | Takes a Sortable list and returns True if the list is sorted in ascending
 --   order and False otherwise.
 
@@ -17,12 +11,8 @@ import Data.Tensort.Utils.Types (Sortable (..))
 --
 -- >>> isSorted (SortBit [0, 1, 2, 4, 3])
 -- False
-isSorted :: Sortable -> Bool
-isSorted (SortBit []) = True
-isSorted (SortBit [_]) = True
-isSorted (SortBit (x : y : remainingElements)) =
-  lessThanOrEqualBit x y && isSorted (SortBit (y : remainingElements))
-isSorted (SortRec []) = True
-isSorted (SortRec [_]) = True
-isSorted (SortRec (x : y : remainingElements)) =
-  lessThanOrEqualRecord x y && isSorted (SortRec (y : remainingElements))
+isSorted :: [Ordering] -> Bool
+isSorted [] = True
+isSorted [_] = True
+isSorted (x : y : remainingElements) =
+  x <= y && isSorted (y : remainingElements)
