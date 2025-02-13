067626b6936e264dbfe2f5c368d2e4a19801c2f2
kaBeech <contact@kaBeech.com>
2025-02-12 17:10:35 -0800

    Simplify Bit and Record sorting in bubblesort

--- src/Data/Tensort/Subalgorithms/Bubblesort.hs
+++ src/Data/Tensort/Subalgorithms/Bubblesort.hs
@@ -1,13 +1,7 @@
--- | This module provides the bubblesort function for sorting Sortable lists
+-- | This module provides the bubblesort function for sorting Bit lists
 module Data.Tensort.Subalgorithms.Bubblesort (bubblesort) where
 
-import Data.Tensort.Utils.ComparisonFunctions
-  ( greaterThanBit,
-    greaterThanRecord,
-  )
-import Data.Tensort.Utils.Types (Sortable (..))
-
--- | Takes a Sortable and returns a sorted Sortable using a Bubblesort
+-- | Takes a Bit and returns a sorted Bit using a Bubblesort
 --   algorithm.
 
 -- | ==== __Examples__
@@ -16,29 +10,25 @@ import Data.Tensort.Utils.Types (Sortable (..))
 --
 -- >>> bubblesort (SortRec [(1, 16), (5, 23), (2, 4), (3, 8), (0, 15), (4, 42)])
 -- SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
-bubblesort :: Sortable -> Sortable
-bubblesort (SortBit bits) =
-  SortBit $
-    bublesortIterable greaterThanBit bits 0 (length bits)
-bubblesort (SortRec recs) =
-  SortRec $
-    bublesortIterable greaterThanRecord recs 0 (length recs)
+bubblesort :: (Ord a) => [a] -> [a]
+bubblesort bits =
+  bublesort' bits 0 (length bits)
 
-bublesortIterable :: (Ord a) => (a -> a -> Bool) -> [a] -> Int -> Int -> [a]
-bublesortIterable greaterThan xs currentIndex i
+bublesort' :: (Ord a) => [a] -> Int -> Int -> [a]
+bublesort' xs currentIndex i
   | length xs < 2 = xs
   | i < 1 =
       xs
   | currentIndex > length xs - 2 =
-      bublesortIterable greaterThan xs 0 (i - 1)
+      bublesort' xs 0 (i - 1)
   | otherwise =
       if leftElemGreater
-        then bublesortIterable greaterThan swappedXs (currentIndex + 1) i
-        else bublesortIterable greaterThan xs (currentIndex + 1) i
+        then bublesort' swappedXs (currentIndex + 1) i
+        else bublesort' xs (currentIndex + 1) i
   where
     left = take currentIndex xs
     right = drop (currentIndex + 2) xs
     x = xs !! currentIndex
     y = xs !! (currentIndex + 1)
-    leftElemGreater = greaterThan x y
+    leftElemGreater = x > y
     swappedXs = left ++ [y] ++ [x] ++ right
