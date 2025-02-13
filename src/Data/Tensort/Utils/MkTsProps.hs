488c094ca878b2084e508487db91293923e5cbd6
kaBeech <contact@kaBeech.com>
2025-02-12 18:32:54 -0800

    Simplify type definition

--- src/Data/Tensort/Utils/MkTsProps.hs
+++ src/Data/Tensort/Utils/MkTsProps.hs
@@ -1,8 +1,8 @@
 -- | This module provides the mkTsProps function for creating TensortProps.
 module Data.Tensort.Utils.MkTsProps (mkTsProps) where
 
-import Data.Tensort.Utils.Types (SortAlg, TensortProps (..))
+import Data.Tensort.Utils.Types (TensortProps (..))
 
 -- | Wraps in integer Bytesize and a SubAlgorithm together as TensortProps.
-mkTsProps :: Int -> SortAlg -> TensortProps
+mkTsProps :: (Ord a) => Int -> ([a] -> [a]) -> TensortProps
 mkTsProps bSize subAlg = TensortProps {bytesize = bSize, subAlgorithm = subAlg}
