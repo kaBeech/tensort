c21500a50c8224a4cc5578b1b2610d7ae87ede1e
kaBeech <contact@kaBeech.com>
2025-02-12 17:17:51 -0800

    Simplify Bit and Record sorting in Magicsort

--- src/Data/Tensort/Subalgorithms/Magicsort.hs
+++ src/Data/Tensort/Subalgorithms/Magicsort.hs
@@ -1,4 +1,4 @@
--- | This module provides the magicsort function for sorting Sortable lists
+-- | This module provides the magicsort function for sorting lists
 module Data.Tensort.Subalgorithms.Magicsort
   ( magicsort,
   )
@@ -6,31 +6,24 @@ where
 
 import Data.Tensort.Subalgorithms.Bogosort (bogosort)
 import Data.Tensort.Subalgorithms.Permutationsort (permutationsort)
-import Data.Tensort.Utils.Types (Sortable (..))
 
--- | Takes a Sortable and returns a sorted Sortable.
+-- | Takes a list and returns a sorted list.
 --
---   Runs both Permutationsort and Bogosort on the input Sortable and compares
+--   Runs both Permutationsort and Bogosort on the input list and compares
 --   the results. If the results agree, returns the result of Permutationsort,
 --   otherwise repeats the process.
 
 -- | ==== __Examples__
--- >>> magicsort (SortBit [16, 23, 4, 8, 15, 42])
+-- >>> magicsort [16, 23, 4, 8, 15, 42]
 -- SortBit [4,8,15,16,23,42]
 --
--- >>> magicsort (SortRec [(1, 16), (5, 23), (2, 4) ,(3, 8), (0, 15), (4, 42)])
--- SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
-magicsort :: Sortable -> Sortable
+-- >>> magicsort [(1, 16), (5, 23), (2, 4) ,(3, 8), (0, 15), (4, 42)]
+-- [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
+magicsort :: (Ord a) => a -> a
 magicsort xs =
-  if verifyResults result1 result2
+  if result1 == result2
     then result1
     else magicsort xs
   where
     result1 = permutationsort xs
     result2 = bogosort xs
-
-verifyResults :: Sortable -> Sortable -> Bool
-verifyResults (SortBit xs) (SortBit ys) = xs == ys
-verifyResults (SortRec xs) (SortRec ys) = map snd xs == map snd ys
-verifyResults (SortBit _) (SortRec _) = False
-verifyResults (SortRec _) (SortBit _) = False
