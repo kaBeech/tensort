f0f0d5152ff81fe0e686ff81619270410a84f413
kaBeech <contact@kaBeech.com>
2025-02-12 17:11:02 -0800

    Fix typo

--- src/Data/Tensort/Subalgorithms/Bubblesort.hs
+++ src/Data/Tensort/Subalgorithms/Bubblesort.hs
@@ -12,19 +12,19 @@ module Data.Tensort.Subalgorithms.Bubblesort (bubblesort) where
 -- SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
 bubblesort :: (Ord a) => [a] -> [a]
 bubblesort bits =
-  bublesort' bits 0 (length bits)
+  bubblesort' bits 0 (length bits)
 
-bublesort' :: (Ord a) => [a] -> Int -> Int -> [a]
-bublesort' xs currentIndex i
+bubblesort' :: (Ord a) => [a] -> Int -> Int -> [a]
+bubblesort' xs currentIndex i
   | length xs < 2 = xs
   | i < 1 =
       xs
   | currentIndex > length xs - 2 =
-      bublesort' xs 0 (i - 1)
+      bubblesort' xs 0 (i - 1)
   | otherwise =
       if leftElemGreater
-        then bublesort' swappedXs (currentIndex + 1) i
-        else bublesort' xs (currentIndex + 1) i
+        then bubblesort' swappedXs (currentIndex + 1) i
+        else bubblesort' xs (currentIndex + 1) i
   where
     left = take currentIndex xs
     right = drop (currentIndex + 2) xs
