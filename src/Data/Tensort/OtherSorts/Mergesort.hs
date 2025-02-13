7ee8aaae487e932cca8ee79aa174e16e6451cd1e
kaBeech <contact@kaBeech.com>
2025-02-12 18:26:02 -0800

    Simplify Bit and Record sorting in Mergesort

--- src/Data/Tensort/OtherSorts/Mergesort.hs
+++ src/Data/Tensort/OtherSorts/Mergesort.hs
@@ -1,55 +1,36 @@
--- | This module provides an implementation of the mergesort algorithm suitable
---   for sorting lists using the Sortable type.
+-- | This module provides an implementation of the mergesort algorithm.
 module Data.Tensort.OtherSorts.Mergesort (mergesort) where
 
-import Data.Tensort.Utils.ComparisonFunctions (lessThanBit, lessThanRecord)
-import Data.Tensort.Utils.Types (Bit, Record, Sortable (..))
+import Data.Tensort.Utils.Types (Bit, Record)
 
--- | Takes a Sortable and returns a sorted Sortable using a Mergesort
+-- | Takes a list and returns a sorted list using a Mergesort
 --   algorithm.
 
 -- | ==== __Examples__
---  >>> mergesort (SortBit [16, 23, 4, 8, 15, 42])
---  SortBit [4,8,15,16,23,42]
+--  >>> mergesort [16, 23, 4, 8, 15, 42]
+--  [4,8,15,16,23,42]
 --
---  >>> mergesort (SortRec [(1, 16), (5, 23), (2, 4) ,(3, 8), (0, 15) , (4, 42)])
---  SortRec [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
-mergesort :: Sortable -> Sortable
-mergesort (SortBit xs) = SortBit (mergesortBits xs)
-mergesort (SortRec xs) = SortRec (mergesortRecs xs)
-
-mergesortBits :: [Bit] -> [Bit]
-mergesortBits = mergeAllBits . map (: [])
+--  >>> mergesort [(1, 16), (5, 23), (2, 4) ,(3, 8), (0, 15) , (4, 42)]
+--  [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
+mergesort :: (Ord a) => [a] -> [a]
+mergesort = mergesortStack . map (: [])
   where
-    mergeAllBits [] = []
-    mergeAllBits [x] = x
-    mergeAllBits [x, y] = mergeBits x y
-    mergeAllBits remaningElements = mergeAllBits (mergePairs remaningElements)
+    mergesortStack [] = []
+    mergesortStack [l] = l
+    mergesortStack [l1, l2] = mergeLists l1 l2
+    mergesortStack remaningElements =
+      mergesortStack
+        (mergePairsOfLists remaningElements)
 
-    mergePairs (x : y : remaningElements) = mergeBits x y : mergePairs remaningElements
-    mergePairs x = x
+    mergePairsOfLists [] = []
+    mergePairsOfLists [l1] = [l1]
+    mergePairsOfLists [l1, l2] = mergeLists l1 l2
+    mergePairsOfLists (l1 : l2 : remaningElements) =
+      mergeLists l1 l2 : mergePairsOfLists remaningElements
 
-mergeBits :: [Bit] -> [Bit] -> [Bit]
-mergeBits [] y = y
-mergeBits x [] = x
-mergeBits (x : xs) (y : ys)
-  | lessThanBit x y = x : mergeBits xs (y : ys)
-  | otherwise = y : mergeBits (x : xs) ys
-
-mergesortRecs :: [Record] -> [Record]
-mergesortRecs = mergeAllRecs . map (: [])
-  where
-    mergeAllRecs [] = []
-    mergeAllRecs [x] = x
-    mergeAllRecs [x, y] = mergeRecs x y
-    mergeAllRecs remaningElements = mergeAllRecs (mergePairs remaningElements)
-
-    mergePairs (x : y : remaningElements) = mergeRecs x y : mergePairs remaningElements
-    mergePairs x = x
-
-mergeRecs :: [Record] -> [Record] -> [Record]
-mergeRecs [] y = y
-mergeRecs x [] = x
-mergeRecs (x : xs) (y : ys)
-  | lessThanRecord x y = x : mergeRecs xs (y : ys)
-  | otherwise = y : mergeRecs (x : xs) ys
+mergeLists :: (Ord a) => [a] -> [a] -> [a]
+mergeLists [] y = y
+mergeLists x [] = x
+mergeLists (x : xs) (y : ys)
+  | x < y = x : mergeLists xs (y : ys)
+  | otherwise = y : mergeLists (x : xs) ys
