56203f0cda966e71e1fe3697335e8be24e3dfef3
kaBeech <contact@kaBeech.com>
2025-02-13 21:12:30 -0800

    Handle type changes in Reduce

--- src/Data/Tensort/Utils/Reduce.hs
+++ src/Data/Tensort/Utils/Reduce.hs
@@ -29,7 +29,7 @@ import Data.Tensort.Utils.Types
 -- >>> import Data.Tensort.Utils.MkTsProps (mkTsProps)
 -- >>> reduceTensorStacks (mkTsProps 2 bubblesort) (STensorsBit [([(0, 33), (1, 38)], ByteMem [[31, 33], [35, 38]]), ([(0, 34), (1, 37)], ByteMem [[32, 14], [36, 37]]), ([(0, 23), (1, 27)], ByteMem [[21, 23], [25, 27]]), ([(0, 24), (1, 28)], ByteMem [[22, 24], [26, 28]]),([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]]),([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])])
 -- STensorBit ([(1,18),(0,38)],TensorMem [([(0,28),(1,38)],TensorMem [([(0,27),(1,28)],TensorMem [([(0,23),(1,27)],ByteMem [[21,23],[25,27]]),([(0,24),(1,28)],ByteMem [[22,24],[26,28]])]),([(1,37),(0,38)],TensorMem [([(0,33),(1,38)],ByteMem [[31,33],[35,38]]),([(0,34),(1,37)],ByteMem [[32,14],[36,37]])])]),([(0,8),(1,18)],TensorMem [([(0,7),(1,8)],TensorMem [([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])]),([(1,17),(0,18)],TensorMem [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]])])])])
-reduceTensorStacks :: TensortProps -> [TensorStack] -> TensorStack
+reduceTensorStacks :: (Ord a) => TensortProps a -> [TensorStack a] -> TensorStack a
 reduceTensorStacks tsProps tensorStacks =
   if length newTensorStacks <= bytesize tsProps
     then createTensor subAlgR memory
@@ -51,14 +51,14 @@ reduceTensorStacks tsProps tensorStacks =
 -- >>> reduceTensorStacksSinglePass (mkTsProps 2 bubblesort) [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]]),([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])]
 -- [([(0,7),(1,8)],TensorMem [([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])]),([(1,17),(0,18)],TensorMem [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]])])]
 reduceTensorStacksSinglePass ::
-  TensortProps ->
-  [TensorStack] ->
-  [TensorStack]
+  (Ord a) => 
+  TensortProps a ->
+  [TensorStack a] ->
+  [TensorStack a]
 reduceTensorStacksSinglePass tsProps tensorStacks =
   foldr acc [] tensorStacks'
   where
     tensorStacks' = splitEvery (bytesize tsProps) tensorStacks
-    acc :: [TensorStack] -> [TensorStack] -> [TensorStack]
     acc tensorStack newTensorStacks =
       newTensorStacks
         ++ [newTensorStack]
