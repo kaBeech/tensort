496ae567b4ab6867609c90bb97be27f7997c62ca
kaBeech <contact@kaBeech.com>
2025-02-13 21:10:52 -0800

    Handle type changes in Render

--- src/Data/Tensort/Utils/Render.hs
+++ src/Data/Tensort/Utils/Render.hs
@@ -15,10 +15,12 @@ import Data.Tensort.Utils.Types
   ( Bit,
     Memory (..),
     Record,
+    Register,
     SortAlg,
     Tensor,
     TensorStack,
     fromJust,
+    fromRecord,
   )
 
 -- | Compile a sorted list of Bits from a list of TensorStacks.
@@ -29,10 +31,9 @@ import Data.Tensort.Utils.Types
 -- [SBitBit 1,SBitBit 3,SBitBit 5,SBitBit 7]
 -- >>> getSortedBits bubblesort (STensorBit ([(0,8),(1,18)],TensorMem [([(0,7),(1,8)],TensorMem [([(0,3),(1,7)],ByteMem [[1,3],[5,7]]),([(0,4),(1,8)],ByteMem [[2,4],[6,8]])]),([(1,17),(0,18)],TensorMem [([(0,13),(1,18)],ByteMem [[11,13],[15,18]]),([(0,14),(1,17)],ByteMem [[12,14],[16,17]])])]))
 -- [SBitBit 1,SBitBit 2,SBitBit 3,SBitBit 4,SBitBit 5,SBitBit 6,SBitBit 7,SBitBit 8,SBitBit 11,SBitBit 12,SBitBit 13,SBitBit 14,SBitBit 15,SBitBit 16,SBitBit 17,SBitBit 18]
-getSortedBits :: SortAlg Bit -> SortAlg Record -> TensorStack -> [Bit]
+getSortedBits :: (Ord a) => SortAlg (Bit a) -> SortAlg (Record a) -> TensorStack a -> [Bit a]
 getSortedBits subAlgB subAlgR tensorRaw = acc tensorRaw []
   where
-    acc :: TensorStack -> [Bit] -> [Bit]
     acc tensor sortedBits =
       if isNothing tensor'
         then nextBit' : sortedBits
@@ -50,7 +51,7 @@ getSortedBits subAlgB subAlgR tensorRaw = acc tensorRaw []
 --   >>> import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
 --   >>> removeTopBit bubblesort ([(0,5),(1,7)],ByteMem [[1,5],[3,7]])
 --   (7,Just ([(1,3),(0,5)],ByteMem [[1,5],[3]]))
-removeTopBit :: SortAlg Bit -> SortAlg Record -> Tensor -> (Bit, Maybe Tensor)
+removeTopBit :: (Ord a) => SortAlg (Bit a) -> SortAlg (Record a) -> (Register a, Memory a) -> (Bit a, Maybe (Tensor a))
 removeTopBit subAlgB subAlgR (register, memory) =
   if isNothing memory'
     then (topBit, Nothing)
@@ -58,12 +59,12 @@ removeTopBit subAlgB subAlgR (register, memory) =
   where
     (topBit, memory') = removeBit subAlgB subAlgR memory topAddress
     topRecord = last register
-    topAddress = snd topRecord
+    topAddress = snd $ fromRecord topRecord
     tensor = Just tensorRaw
     tensorRaw = createTensor subAlgR memRefined
     memRefined = fromJust memory'
 
-removeBit :: SortAlg Bit -> SortAlg Record -> Memory -> Int -> (Bit, Maybe Memory)
+removeBit :: (Ord a) => SortAlg (Bit a) -> SortAlg (Record a) -> Memory a -> Int -> (Bit a, Maybe (Memory a))
 removeBit subAlgB _ (ByteMem bytes) i =
   case length topByte' of
     0 ->
