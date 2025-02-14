96229a64ae301ca787cb14f64c182decb2958578
kaBeech <contact@kaBeech.com>
2025-02-14 10:26:20 -0800

    Add DocTest

--- src/Data/Tensort/Utils/Split.hs
+++ src/Data/Tensort/Utils/Split.hs
@@ -3,6 +3,10 @@
 module Data.Tensort.Utils.Split (splitEvery) where
 
 -- | Split a list into chunks of a given size.
+
+-- | ==== __Examples__
+--   >>> splitEvery 2 [1,2,3,4,5,6]
+--   [[1,2],[3,4],[5,6]]
 splitEvery :: Int -> [a] -> [[a]]
 splitEvery _ [] = []
 splitEvery n xs = take n xs : splitEvery n (drop n xs)
