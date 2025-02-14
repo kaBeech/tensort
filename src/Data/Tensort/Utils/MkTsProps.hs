0129e136c804e3a968a212d672dd93de5b45faae
kaBeech <contact@kaBeech.com>
2025-02-13 21:38:07 -0800

    Use one subAlgorithm for both Bits and Recs

--- src/Data/Tensort/Utils/MkTsProps.hs
+++ src/Data/Tensort/Utils/MkTsProps.hs
@@ -1,8 +1,8 @@
 -- | This module provides the mkTsProps function for creating TensortProps.
 module Data.Tensort.Utils.MkTsProps (mkTsProps) where
 
-import Data.Tensort.Utils.Types (Bit, Record, SortAlg, TensortProps (..))
+import Data.Tensort.Utils.Types (SortAlg, TensortProps (..))
 
--- | Wraps in integer Bytesize and a SubAlgorithm together as TensortProps.
-mkTsProps :: (Ord a) => Int -> SortAlg (Bit a) -> SortAlg (Record a) -> TensortProps a
-mkTsProps bSize subAlgB subAlgR = TensortProps {bytesize = bSize, subAlgorithmBits = subAlgB, subAlgorithmRecs = subAlgR}
+-- | Wraps an integer Bytesize and a SubAlgorithm together as TensortProps.
+mkTsProps :: (Ord a) => Int -> SortAlg a -> TensortProps a
+mkTsProps bSize subAlg = TensortProps {bytesize = bSize, subAlgorithm = subAlg}
