6cf9b3bcae219da5ad85ea2975aec4a5bd53808e
kaBeech <contact@kaBeech.com>
2025-02-13 23:46:19 -0800

    Fix DocTests in Bogosort

--- src/Data/Tensort/Subalgorithms/Bogosort.hs
+++ src/Data/Tensort/Subalgorithms/Bogosort.hs
@@ -7,11 +7,11 @@ import Data.Tensort.Utils.RandomizeList (randomizeList)
 -- | Takes a list and returns a sorted list using a Bogosort algorithm.
 
 -- | ==== __Examples__
--- >>> bogosort [16, 23, 4, 8, 15, 42]
+-- >>> bogosort ([16, 23, 4, 8, 15, 42] :: [Int])
 -- [4,8,15,16,23,42]
 --
--- >>> bogosort [(1, 16), (5, 23), (2, 4), (3, 8), (0, 15), (4, 42)]
--- [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
+-- >>> bogosort ([(1, 16), (5, 23), (2, 4), (3, 8), (0, 15), (4, 42)] :: [(Int, Int)])
+-- [(0,15),(1,16),(2,4),(3,8),(4,42),(5,23)]
 bogosort :: (Ord a) => [a] -> [a]
 bogosort = bogosortSeeded 143
 
@@ -19,11 +19,11 @@ bogosort = bogosortSeeded 143
 --  sorted list using a Bogosort algorithm.
 
 -- | ==== __Examples__
--- >>> bogosortSeeded 42 [16, 23, 4, 8, 15, 42]
+-- >>> bogosortSeeded 42 ([16, 23, 4, 8, 15, 42] :: [Int])
 -- [4,8,15,16,23,42]
 --
--- >>> bogosortSeeded 24 [(1, 16), (5, 23), (2, 4), (3, 8), (0, 15), (4, 42)]
--- [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
+-- >>> bogosortSeeded 24 ([(1, 16), (5, 23), (2, 4), (3, 8), (0, 15), (4, 42)] :: [(Int, Int)])
+-- [(0,15),(1,16),(2,4),(3,8),(4,42),(5,23)]
 bogosortSeeded :: (Ord a) => Int -> [a] -> [a]
 bogosortSeeded seed xs
   | isSorted xs = xs
