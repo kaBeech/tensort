47b935051224c122076c5d07929de09e812936cb
kaBeech <contact@kaBeech.com>
2025-02-13 21:17:02 -0800

    Make Mergesort a bit easier to comprehend

--- src/Data/Tensort/OtherSorts/Mergesort.hs
+++ src/Data/Tensort/OtherSorts/Mergesort.hs
@@ -1,8 +1,6 @@
 -- | This module provides an implementation of the mergesort algorithm.
 module Data.Tensort.OtherSorts.Mergesort (mergesort) where
 
-import Data.Tensort.Utils.Types (Bit, Record)
-
 -- | Takes a list and returns a sorted list using a Mergesort
 --   algorithm.
 
@@ -14,19 +12,21 @@ import Data.Tensort.Utils.Types (Bit, Record)
 --  [(2,4),(3,8),(0,15),(1,16),(5,23),(4,42)]
 mergesort :: (Ord a) => [a] -> [a]
 mergesort = mergesortStack . map (: [])
-  where
-    mergesortStack [] = []
-    mergesortStack [l] = l
-    mergesortStack [l1, l2] = mergeLists l1 l2
-    mergesortStack remaningElements =
-      mergesortStack
-        (mergePairsOfLists remaningElements)
 
-    mergePairsOfLists [] = []
-    mergePairsOfLists [l1] = [l1]
-    mergePairsOfLists [l1, l2] = mergeLists l1 l2
-    mergePairsOfLists (l1 : l2 : remaningElements) =
-      mergeLists l1 l2 : mergePairsOfLists remaningElements
+mergesortStack :: (Ord a) => [[a]] -> [a]
+mergesortStack [] = []
+mergesortStack [l] = l
+mergesortStack [l1, l2] = mergeLists l1 l2
+mergesortStack remaningElements =
+  mergesortStack
+    (mergePairsOfLists remaningElements)
+
+mergePairsOfLists :: (Ord a) => [[a]] -> [[a]]
+mergePairsOfLists [] = []
+mergePairsOfLists [l1] = [l1]
+mergePairsOfLists [l1, l2] = [mergeLists l1 l2]
+mergePairsOfLists (l1 : l2 : remaningElements) =
+  mergeLists l1 l2 : mergePairsOfLists remaningElements
 
 mergeLists :: (Ord a) => [a] -> [a] -> [a]
 mergeLists [] y = y
