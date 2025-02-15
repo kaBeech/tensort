a4fb1a710f86c16db048c577af6aa769a008e5b6
kaBeech <contact@kaBeech.com>
2025-02-14 17:22:18 -0800

    Simplify

--- src/Data/Tensort/Dimensionsort/Convert.hs
+++ src/Data/Tensort/Dimensionsort/Convert.hs
@@ -14,12 +14,14 @@ import Data.Tensort.Utils.Split (splitEvery)
 --   >>> import Data.Tensort.Dimensionsort.MkDsProps (mkDsProps)
 --   >>> rawElemsToUniverses (mkDsProps 4 bubblesort) ([5,1,3,7,8,2,4,6] :: [Int])
 --   [[2,4,6,8],[1,3,5,7]]
-rawElemsToUniverses :: DimensionsortProps a -> Hyperverse a -> [Hyperverse a]
-rawElemsToUniverses dsProps (Universe elems) = foldr acc [] universes
+rawElemsToUniverses :: DimensionsortProps a -> Hyperverse a -> Hyperverse a
+rawElemsToUniverses dsProps (Universe elems) = Megaverse sortedUniverses
   where
     universes = map Universe $ splitEvery (versesize dsProps) elems
-    acc universe universesSorted =
-      universesSorted ++ [universeSorted]
-      where
-        universeSorted = subAlgorithm dsProps universe
-rawElemsToUniverses _ (Megaverse _) = error "rawElemsToUniverses: Megaverse input not supported"
+    subAlg = subAlgorithm dsProps
+    sortedUniverses = map subAlg universes
+rawElemsToUniverses dsProps (Megaverse verses) = Megaverse sortedVerses
+  where
+    megaverses = map Megaverse $ splitEvery (versesize dsProps) verses
+    subAlg = subAlgorithm dsProps
+    sortedVerses = map subAlg megaverses
