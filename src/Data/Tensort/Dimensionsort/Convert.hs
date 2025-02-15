c230f13e12c9ae34fafc8a6e96b71b025eb978ad
kaBeech <contact@kaBeech.com>
2025-02-14 17:42:13 -0800

    Add condenseMegaverse

--- src/Data/Tensort/Dimensionsort/Convert.hs
+++ src/Data/Tensort/Dimensionsort/Convert.hs
@@ -1,4 +1,4 @@
-module Data.Tensort.Dimensionsort.Convert (rawElemsToUniverses) where
+module Data.Tensort.Dimensionsort.Convert (condenseHyperverses, condenseMegaverse) where
 
 import Data.Tensort.Dimensionsort.Types
   ( DimensionsortProps (..),
@@ -6,21 +6,31 @@ import Data.Tensort.Dimensionsort.Types
   )
 import Data.Tensort.Utils.Split (splitEvery)
 
+condenseMegaverse :: (Eq a, Show a, Ord a) => DimensionsortProps a -> Hyperverse a -> Hyperverse a
+condenseMegaverse dsProps (Universe megaverse) =
+  if length megaverse == 1
+    then Universe megaverse
+    else condenseMegaverse dsProps $ condenseHyperverses dsProps $ Universe megaverse
+condenseMegaverse dsProps (Megaverse megaverse) =
+  if length megaverse == 1
+    then head megaverse
+    else condenseMegaverse dsProps $ condenseHyperverses dsProps $ Megaverse megaverse
+
 -- | Convert a list of Elems to a list of Universes of given versesize, sorting
 --   each Universe with the given subalgorithm.
 
 -- | ==== __Examples__
 --   >>> import Data.Tensort.Subalgorithms.Bubblesort (bubblesort)
 --   >>> import Data.Tensort.Dimensionsort.MkDsProps (mkDsProps)
---   >>> rawElemsToUniverses (mkDsProps 4 bubblesort) ([5,1,3,7,8,2,4,6] :: [Int])
+--   >>> condenseHyperverses (mkDsProps 4 bubblesort) ([5,1,3,7,8,2,4,6] :: [Int])
 --   [[2,4,6,8],[1,3,5,7]]
-rawElemsToUniverses :: DimensionsortProps a -> Hyperverse a -> Hyperverse a
-rawElemsToUniverses dsProps (Universe elems) = Megaverse sortedUniverses
+condenseHyperverses :: DimensionsortProps a -> Hyperverse a -> Hyperverse a
+condenseHyperverses dsProps (Universe elems) = Megaverse sortedUniverses
   where
     universes = map Universe $ splitEvery (versesize dsProps) elems
     subAlg = subAlgorithm dsProps
     sortedUniverses = map subAlg universes
-rawElemsToUniverses dsProps (Megaverse verses) = Megaverse sortedVerses
+condenseHyperverses dsProps (Megaverse verses) = Megaverse sortedVerses
   where
     megaverses = map Megaverse $ splitEvery (versesize dsProps) verses
     subAlg = subAlgorithm dsProps
